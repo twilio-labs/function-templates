dcc5cc35302ecacae89cdd04dc27158474864d59
Phil Nash <philnash@gmail.com>
2017-07-05 13:06:02 +0100

    Adds message forwarder

---
+++ forward-message/README.md
@@ -0,0 +1,15 @@
+# Forward Message
+
+This Function will return the TwiML required to forward an incoming SMS message to a number that is set in the environment variables.
+
+## Environment variables
+
+This Function expects one environment variable to be set.
+
+| Variable          | Meaning |
+| :---------------- | :------ |
+| `MY_PHONE_NUMBER` | The number you want to forward incoming messages to [in E.164 format](https://support.twilio.com/hc/en-us/articles/223183008-Formatting-International-Phone-Numbers) |
+
+## Parameters
+
+This Function expects the incoming request to be a messaging webhook. The parameters that will be used are `From` and `Body`.
---
+++ forward-message/forward-message.test.js
@@ -0,0 +1,48 @@
+const helpers = require('../test/test-helper');
+const forwardMessage = require('./forward-message').handler;
+const Twilio = require('twilio');
+
+const context = {
+  MY_PHONE_NUMBER: 'TwilioNumber'
+};
+const event = {
+  Body: 'Hello',
+  From: 'ExternalNumber'
+};
+
+beforeAll(() => {
+  helpers.setup(context);
+});
+
+afterAll(() => {
+  helpers.teardown();
+});
+
+test('returns a MessagingResponse', done => {
+  const callback = (err, result) => {
+    expect(result).toBeInstanceOf(Twilio.twiml.MessagingResponse);
+    done();
+  };
+
+  forwardMessage(context, event, callback);
+});
+
+test('forwards the message to the number from the context', done => {
+  const callback = (err, result) => {
+    expect(result.toString()).toMatch('to="' + context.MY_PHONE_NUMBER + '"');
+    done();
+  };
+
+  forwardMessage(context, event, callback);
+});
+
+test('includes the original From number and Body', done => {
+  const callback = (err, result) => {
+    expect(result.toString()).toMatch(
+      '>From: ' + event.From + '. Body: ' + event.Body + '<'
+    );
+    done();
+  };
+
+  forwardMessage(context, event, callback);
+});
