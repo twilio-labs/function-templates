b040b2008a7150177fefef8ff33b3f70bb5a923a
Dominik Kundel <dominik.kundel@gmail.com>
2019-07-18 11:31:06 -0700

    feat(template): add new play template (#22)

---
+++ never-gonna-give-you-up/never-gonna-give-you-up.js
@@ -0,0 +1,6 @@
+exports.handler = function(context, event, callback) {
+  const twiml = new Twilio.twiml.VoiceResponse();
+  twiml.play('https://demo.twilio.com/docs/classic.mp3');
+
+  callback(null, twiml);
+};
