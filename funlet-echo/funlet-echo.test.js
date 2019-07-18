2c309c7d2379a682f870d88bf83cb02072e2da29
Eric Bréchemier <twilio@eric.brechemier.name>
2019-07-19 00:58:06 +0200

    Define unit tests for Echo Funlet

--- funlet-echo/funlet-echo.test.js
+++ funlet-echo/funlet-echo.test.js
@@ -1,8 +1,26 @@
 const funlet = require('./funlet-echo').handler;
+const TWIML = '<Response><Say>echo okay</Say></Response>';
 
-test.skip('Missing Tests', done => {
+test('[Echo-1.1] Echo (GET)', done => {
   const callback = (err, result) => {
-    expect(result).toBe('...');
+    expect(result).toBe( TWIML );
+    done();
+  };
+  funlet({}, {Twiml:TWIML}, callback);
+});
+
+test('[Echo-1.2] Echo (ENV)', done => {
+  const callback = (err, result) => {
+    expect(result).toBe( TWIML );
+    done();
+  };
+  funlet({ECHO_TWIML:TWIML}, {}, callback);
+});
+
+test('[Echo-1.3] Echo (Default)', done => {
+  const DEFAULT_TWIML = '<Response><Say>echo</Say></Response>';
+  const callback = (err, result) => {
+    expect(result).toBe( DEFAULT_TWIML );
     done();
   };
   funlet({}, {}, callback);
