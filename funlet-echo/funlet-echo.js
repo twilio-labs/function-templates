a7b596db171a1581ce4d0caab555f3ca930e3616
Eric Bréchemier <twilio@eric.brechemier.name>
2019-07-19 22:44:26 +0200

    Rewrite Echo Funlet to set proper Content-Type for output

--- funlet-echo/funlet-echo.js
+++ funlet-echo/funlet-echo.js
@@ -1,5 +1,15 @@
 const MY_TWIML = '<Response><Say>echo</Say></Response>';
 
+const Twilio = require('twilio');
+
 exports.handler = function(context, event, callback) {
-  callback(null, event.Twiml || context.ECHO_TWIML || MY_TWIML );
+  // Create a custom response in XML format
+  let response = new Twilio.Response();
+  response.appendHeader("Content-Type", "text/xml");
+
+  // Use input TwiML as output
+  response.setBody( event.Twiml || context.ECHO_TWIML || MY_TWIML )
+
+  // Return the response
+  callback(null, response);
 };
--- funlet-echo/funlet-echo.test.js
+++ funlet-echo/funlet-echo.test.js
@@ -1,9 +1,17 @@
 const funlet = require('./funlet-echo').handler;
+const Twilio = require('twilio');
 const TWIML = '<Response><Say>echo okay</Say></Response>';
+
+function expectTwiml(result, Twiml) {
+  expect( result ).toBeInstanceOf( Twilio.Response );
+  expect( result.statusCode ).toBe( 200 );
+  expect( result.body ).toBe( TWIML );
+  expect( result.headers['Content-Type'] ).toBe( 'text/xml' );
+}
 
 test('[Echo-1.1] Echo (GET)', done => {
   const callback = (err, result) => {
-    expect(result).toBe( TWIML );
+    expectTwiml(result, TWIML);
     done();
   };
   funlet({}, {Twiml:TWIML}, callback);
@@ -11,7 +19,7 @@ test('[Echo-1.1] Echo (GET)', done => {
 
 test('[Echo-1.2] Echo (ENV)', done => {
   const callback = (err, result) => {
-    expect(result).toBe( TWIML );
+    expectTwiml(result, TWIML);
     done();
   };
   funlet({ECHO_TWIML:TWIML}, {}, callback);
@@ -20,7 +28,7 @@ test('[Echo-1.2] Echo (ENV)', done => {
 test('[Echo-1.3] Echo (Default)', done => {
   const DEFAULT_TWIML = '<Response><Say>echo</Say></Response>';
   const callback = (err, result) => {
-    expect(result).toBe( DEFAULT_TWIML );
+    expectTwiml(result, DEFAULT_TWIML);
     done();
   };
   funlet({}, {}, callback);
