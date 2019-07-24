5b36b099662698e4f27ea91da6aed4f8223c9008
Eric Bréchemier <twilio@eric.brechemier.name>
2019-07-24 22:37:38 +0200

    Apply the same base structure to all the funlets

--- funlet-find-me/funlet-find-me.js
+++ funlet-find-me/funlet-find-me.js
@@ -1,4 +1,16 @@
-exports.handler = function(context, event, callback) {
+// # Find Me Funlet
+
+// ## Input
+exports.input = {};
+
+// ## Dependencies
+const Twilio = require('twilio');
+
+// ## Output
+exports.output = {};
+
+exports.handler = function(env, params, reply) {
+  const NO_ERROR = null;
   throw Error("Not implemented!");
-  callback(null, 'response');
+  reply(NO_ERROR, 'response');
 };
