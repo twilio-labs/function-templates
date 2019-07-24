ceb815470dde1b84597b95b784647558a815ba4d
Eric Bréchemier <twilio@eric.brechemier.name>
2019-07-25 00:50:26 +0200

    Add tests and message() function for output of Simple Message Funlet

--- funlet-simple-message/funlet-simple-message.js
+++ funlet-simple-message/funlet-simple-message.js
@@ -36,6 +36,36 @@ const Twilio = require('twilio');
 // ## Output
 exports.output = {};
 
+/*
+  Function: message(response,message)
+
+  Parameters:
+    * response - Twilio.twiml.VoiceResponse, Twilio Voice response in progress
+    * message - string, recorded message (URL starting with 'http') to play
+                or text message to say
+    * language - string, language for text messages, e.g. 'en' for English
+                 with an American accent or 'en-gb' for English with a British
+                 accent. Use the voice 'alice' for the largest list of
+                 supported languages and accents.
+    * voice - string, one of 'man', 'woman' or 'alice'.
+
+  Response:
+    The input response is modified with further instructions to Play or Say
+    the given message. The input response is returned unchanged when the
+    given message is empty.
+*/
+function message(response, message, language, voice) {
+  if ( message.length === 0 ) {
+    return;
+  }
+  if ( message.startsWith("http") ) {
+    response.play({}, message);
+  } else {
+    response.say({language:language, voice:voice}, message);
+  }
+}
+exports.output.message = message;
+
 exports.handler = function(env, params, reply) {
   const NO_ERROR = null;
   throw Error("Not implemented!");
--- funlet-simple-message/funlet-simple-message.test.js
+++ funlet-simple-message/funlet-simple-message.test.js
@@ -2,12 +2,17 @@ const funlet = require('./funlet-simple-message');
 const Twilio = require('twilio');
 
 const MESSAGE1="Message #1";
-const MESSAGE2="Message #2";
+const MESSAGE2="https://example.com/message2";
 const MESSAGE3="Message #3";
-const MESSAGE4="Message #4";
+const MESSAGE4="https://example.com/message4";
 const MESSAGE5="Message #5";
 
 const DEFAULT_MESSAGE="";
+
+const XML_DECLARATION='<?xml version="1.0" encoding="UTF-8"?>';
+
+const ENGLISH="en";
+const ALICE="alice";
 
 test('[SIMPLE-MESSAGE-INPUT-MESSAGE-1] Read Single Message from Event',
 () => {
@@ -50,6 +55,46 @@ test('[SIMPLE-MESSAGE-INPUT-MESSAGE-5] Read Default Message from Script',
   expect(
     funlet.input.getMessage({}, {})
   ).toEqual( [DEFAULT_MESSAGE] );
+});
+
+test('[SIMPLE-MESSAGE-OUTPUT-MESSAGE-0] message() an Empty Message',
+() => {
+  const EMPTY_MESSAGE="";
+  const EMPTY_RESPONSE=
+    XML_DECLARATION+
+    '<Response/>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  funlet.output.message( response, EMPTY_MESSAGE, ENGLISH, ALICE );
+  expect( response.toString() ).toEqual( EMPTY_RESPONSE );
+});
+
+test('[SIMPLE-MESSAGE-OUTPUT-MESSAGE-1] message() a Recorded Message',
+() => {
+  const RECORDED_MESSAGE="https://example.com/recorded-message";
+  const PLAY_RECORDED_MESSAGE =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Play>'+RECORDED_MESSAGE+'</Play>'+
+    '</Response>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  funlet.output.message(response, RECORDED_MESSAGE, ENGLISH, ALICE);
+  expect( response.toString() ).toEqual( PLAY_RECORDED_MESSAGE );
+});
+
+test('[SIMPLE-MESSAGE-OUTPUT-MESSAGE-2] message() a Text Message',
+() => {
+  const TEXT_MESSAGE="Text message";
+  const SAY_TEXT_MESSAGE =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Say language="'+ENGLISH+'" voice="'+ALICE+'">'+TEXT_MESSAGE+'</Say>'+
+    '</Response>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  funlet.output.message(response, TEXT_MESSAGE, ENGLISH, ALICE);
+  expect( response.toString() ).toEqual( SAY_TEXT_MESSAGE );
 });
 
 test.skip('Missing Tests', done => {
