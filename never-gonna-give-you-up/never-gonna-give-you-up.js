725931188a633951b0daab7f33f05fedfc0cc3dc
stefan judis <stefanjudis@gmail.com>
2019-07-26 14:25:50 +0200

    feat(template): add new forward-call template

--- never-gonna-give-you-up/never-gonna-give-you-up.js
+++
@@ -1,6 +0,0 @@
-exports.handler = function(context, event, callback) {
-  const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.play('https://demo.twilio.com/docs/classic.mp3');
-
-  callback(null, twiml);
-};
