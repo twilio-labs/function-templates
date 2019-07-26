f93fb7fe9719f64a9c6356a11eacf50b1ec550c5
Eric Bréchemier <twilio@eric.brechemier.name>
2019-07-26 19:07:42 +0200

    Extract shell functions to simplify HTTP query and XML indenting

--- funlet-echo/test-echo.sh
+++ funlet-echo/test-echo.sh
@@ -4,7 +4,30 @@
 # Parameter:
 #   $1 - URL of a deployed instance of the Echo Twimlet/Funlet
 #
+# Uses:
+#   * curl - transfer a URL
+#   * xmllint - command line XML tool
+#
 url="$1"
+
+indentXml(){
+  xmllint --format -
+}
+
+# Function: query()
+#
+# Parameters:
+#   $* - list of key or key=value HTTP parameters, properly URL-encoded
+#
+query(){
+  # join parameters with '&'
+  regularIFS="$IFS"
+  IFS=\&
+  params="$*"
+  IFS="$regularIFS"
+
+  curl -s "$url"?"$params" | indentXml
+}
 
 if test -z "$url"
 then
@@ -15,5 +38,4 @@ fi
 
 echo '[Echo-1] Successful Echo'
 twiml='%3CResponse%3E%3CSay%3Eecho%20okay%3C%2FSay%3E%3C%2FResponse%3E'
-curl -s "$url"?Twiml="$twiml" \
-| xmllint --format -
+query Twiml="$twiml"
