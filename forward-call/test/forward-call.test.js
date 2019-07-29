b49d2780c6c1a8ca4cba30a887adf42b470def93
Stefan Judis <stefanjudis@gmail.com>
2019-07-30 01:01:03 +0200

    feat(template): add new forward-call template (#24)
    * feat(template): add new play template (#22)
    
    * feat(template): add new forward-call template
    
    * Reorder templates
    
    * Fix test for forward-call

---
+++ forward-call/test/forward-call.test.js
@@ -0,0 +1,34 @@
+const helpers = require('../../test/test-helper');
+const forwardCall = require('../functions/forward-call').handler;
+const Twilio = require('twilio');
+
+const context = {
+  MY_PHONE_NUMBER: 'TwilioNumber'
+};
+const event = {};
+
+beforeAll(() => {
+  helpers.setup(context);
+});
+
+afterAll(() => {
+  helpers.teardown();
+});
+
+test('returns a VoiceResponse', done => {
+  const callback = (err, result) => {
+    expect(result).toBeInstanceOf(Twilio.twiml.VoiceResponse);
+    done();
+  };
+
+  forwardCall(context, event, callback);
+});
+
+test('forwards the call to the number from the context', done => {
+  const callback = (err, result) => {
+    expect(result.toString()).toMatch('<Dial>' + context.MY_PHONE_NUMBER + '</Dial>');
+    done();
+  };
+
+  forwardCall(context, event, callback);
+});
