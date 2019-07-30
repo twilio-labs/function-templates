d57e013c812c23bb277eef7a30cedbfadb386abe
Eric Bréchemier <twilio@eric.brechemier.name>
2019-07-31 01:39:05 +0200

    Add unit tests and implementation for input/output of Simple Menu Funlet

--- funlet-simple-menu/funlet-simple-menu.js
+++ funlet-simple-menu/funlet-simple-menu.js
@@ -3,11 +3,184 @@
 // ## Input
 exports.input = {};
 
+function getMessage(env, params) {
+  const MY_MESSAGE = "";
+  return params.Message || env.FUNLET_MENU_MESSAGE || MY_MESSAGE;
+}
+exports.input.getMessage = getMessage;
+
+function getLanguage(env, params) {
+  const MY_LANGUAGE = "en";
+  return params.Language || env.FUNLET_MENU_LANGUAGE || MY_LANGUAGE;
+}
+exports.input.getLanguage = getLanguage;
+
+function getVoice(env, params) {
+  const MY_VOICE = "alice";
+  return params.Voice || env.FUNLET_MENU_VOICE || MY_VOICE;
+}
+exports.input.getVoice = getVoice;
+
+function getOptions(env, params) {
+  let options = {};
+  switch( typeof params.Options ) {
+    case "string":
+      options["0"] = params.Options;
+      break;
+    case "object":
+      let objectOrArray = params.Options;
+      for( let name of Object.keys(objectOrArray) ) {
+        if ( !isNaN( name ) ) {
+          options[ name ] = objectOrArray[ name ];
+        }
+      }
+      break;
+  }
+  for( let name of Object.keys(params) ) {
+    let matches = /^Options\[([0-9]+)\]$/.exec( name );
+    if( matches !== null ) {
+      let digits = matches[1];
+      options[ digits ] = params[ name ];
+    }
+  }
+  for( let name of Object.keys(env) ) {
+    let matches = /^FUNLET_MENU_OPTION([0-9]+)_URL$/.exec( name );
+    if( matches !== null ) {
+      let optionNumber = matches[1];
+      let digits = env[ "FUNLET_MENU_OPTION" + optionNumber + "_DIGITS" ]
+      digits = digits || optionNumber;
+      options[ digits ] = env[ name ];
+    }
+  }
+  return options;
+}
+exports.input.getOptions = getOptions;
+
+function getDigits(env, params) {
+  if (typeof params.Digits === "string") {
+    return params.Digits;
+  }
+  return "";
+}
+exports.input.getDigits = getDigits;
+
 // ## Dependencies
 const Twilio = require('twilio');
 
 // ## Output
 exports.output = {};
+
+// Copied from Simple Message Funlet
+function simpleMessage(response, message, language, voice) {
+  if ( message.length === 0 ) {
+    return;
+  }
+  if ( message.startsWith("http") ) {
+    response.play({}, message);
+  } else {
+    response.say({language:language, voice:voice}, message);
+  }
+}
+exports.output.simpleMessage = simpleMessage;
+
+/*
+  Function: gatherDigits()
+
+  Parameters:
+    * response - Twilio.twiml.VoiceResponse, Twilio Voice response in progress
+    * maxDigits - number, maximum number of digits to gather
+    * message - string, recorded message (URL starting with 'http') to play
+                or text message to say
+    * language - string, language for text messages, e.g. 'en' for English
+                 with an American accent or 'en-gb' for English with a British
+                 accent. Use the voice 'alice' for the largest list of
+                 supported languages and accents.
+    * voice - string, voice for text messages,
+              one of 'man', 'woman' or 'alice'.
+
+  Response:
+    The input response is modified with instructions to gather at most the
+    given number of digits and send them to a new instance of this script.
+*/
+function gatherDigits(response, maxDigits, message, language, voice) {
+  simpleMessage(
+    response.gather({numDigits: maxDigits}),
+    message,
+    language,
+    voice
+  );
+}
+exports.output.gatherDigits = gatherDigits;
+
+/*
+  Function: simpleMenuStage1()
+
+  Parameters:
+    * response - Twilio.twiml.VoiceResponse, Twilio Voice response in progress
+    * message - string, recorded message (URL starting with 'http') to play
+                or text message to say
+    * language - string, language for text messages, e.g. 'en' for English
+                 with an American accent or 'en-gb' for English with a British
+                 accent. Use the voice 'alice' for the largest list of
+                 supported languages and accents.
+    * voice - string, voice for text messages,
+              one of 'man', 'woman' or 'alice'.
+    * options - hash of digits -> URL associating digits pressed with the
+                URL of a script returning TwiML instructions for that option
+
+  Response:
+    The input response is modified with instructions to gather at most
+    the maximum number of digits present in given options, and start a
+    new instance of this script, whether any digits were gathered or not.
+*/
+function simpleMenuStage1(response, message, language, voice, options) {
+  let maxDigits = 1;
+  for( let digits of Object.keys(options) ) {
+    maxDigits = Math.max(maxDigits, digits.length);
+  }
+  gatherDigits(response, maxDigits, message, language, voice);
+  response.redirect({},"");
+}
+exports.output.simpleMenuStage1 = simpleMenuStage1;
+
+/*
+  Function: simpleMenuStage2()
+
+  Parameters:
+    * response - Twilio.twiml.VoiceResponse, Twilio Voice response in progress
+    * digits - string of digits gathered
+    * options - hash of digits -> URL associating digits pressed with the
+                URL of a script returning TwiML instructions for that option
+    * errorMessage - string, recorded message (URL starting with 'http') to
+                     play or text message to say in case the digits pressed
+                     do not match one of the options
+    * language - string, language for text messages, e.g. 'en' for English
+                 with an American accent or 'en-gb' for English with a British
+                 accent. Use the voice 'alice' for the largest list of
+                 supported languages and accents.
+    * voice - string, voice for text messages,
+              one of 'man', 'woman' or 'alice'.
+
+  Response:
+    The input response is modified with instructions to redirect to the
+    URL of the option matching the given digits, if any, or to say an
+    error message.
+
+  Returns:
+    boolean, true if a matching option was found, and false otherwise
+*/
+function simpleMenuStage2(
+  response, digits, options, errorMessage, language, voice
+) {
+  if ( !options.hasOwnProperty(digits) ) {
+    simpleMessage(response, errorMessage, language, voice);
+    return false;
+  }
+  let optionUrl = options[ digits ];
+  response.redirect({}, optionUrl);
+  return true;
+}
+exports.output.simpleMenuStage2 = simpleMenuStage2;
 
 exports.handler = function(env, params, reply) {
   const NO_ERROR = null;
--- funlet-simple-menu/funlet-simple-menu.test.js
+++ funlet-simple-menu/funlet-simple-menu.test.js
@@ -1,9 +1,289 @@
-const funlet = require('./funlet-simple-menu').handler;
+const funlet = require('./funlet-simple-menu');
+const Twilio = require('twilio');
+
+const TEXT_MESSAGE="Text message";
+const MESSAGE=TEXT_MESSAGE;
+const DEFAULT_MESSAGE="";
+
+const ENGLISH="en";
+const FRENCH="fr";
+const DEFAULT_LANGUAGE=ENGLISH;
+
+const MAN="man";
+const WOMAN="woman";
+const ALICE="alice";
+const DEFAULT_VOICE=ALICE;
+
+const ACTION0="https://example.com/options/0";
+const ACTION1="https://example.com/options/1";
+const ACTION2="https://example.com/options/2";
+const ACTION7="https://example.com/options/7";
+const ACTION42="https://example.com/options/42";
+const ACTION101="https://example.com/options/101";
+const ACTION1234="https://example.com/options/1234";
+const ACTION12345="https://example.com/options/12345";
+
+const DEFAULT_OPTIONS={};
+
+const NO_DIGITS="";
+const NON_EMPTY_DIGITS="123";
+
+const XML_DECLARATION='<?xml version="1.0" encoding="UTF-8"?>';
+
+const ERROR_MESSAGE="https://example.com/simple-menu/error.mp3";
+
+test('[SIMPLE-MENU-INPUT-MESSAGE-1] Read Message from Event',
+() => {
+  expect(
+    funlet.input.getMessage({}, {Message:MESSAGE})
+  ).toEqual( MESSAGE );
+});
+
+test('[SIMPLE-MENU-INPUT-MESSAGE-2] Read Message from Environment',
+() => {
+  expect(
+    funlet.input.getMessage({FUNLET_MENU_MESSAGE:MESSAGE}, {})
+  ).toEqual( MESSAGE );
+});
+
+test('[SIMPLE-MENU-INPUT-MESSAGE-3] Read Default Message from Script',
+() => {
+  expect(
+    funlet.input.getMessage({}, {})
+  ).toEqual( DEFAULT_MESSAGE );
+});
+
+test('[SIMPLE-MENU-INPUT-LANGUAGE-1] Read Language from Event',
+() => {
+  expect(
+    funlet.input.getLanguage({}, {Language:FRENCH})
+  ).toEqual( FRENCH );
+});
+
+test('[SIMPLE-MENU-INPUT-LANGUAGE-2] Read Language from Environment',
+() => {
+  expect(
+    funlet.input.getLanguage({FUNLET_MENU_LANGUAGE:FRENCH}, {})
+  ).toEqual( FRENCH );
+});
+
+test('[SIMPLE-MENU-INPUT-LANGUAGE-3] Read Default Language from Script',
+() => {
+  expect(
+    funlet.input.getLanguage({}, {})
+  ).toEqual( DEFAULT_LANGUAGE );
+});
+
+test('[SIMPLE-MENU-INPUT-VOICE-1] Read Voice from Event',
+() => {
+  expect(
+    funlet.input.getVoice({}, {Voice:MAN})
+  ).toEqual( MAN );
+});
+
+test('[SIMPLE-MENU-INPUT-VOICE-2] Read Voice from Environment',
+() => {
+  expect(
+    funlet.input.getVoice({FUNLET_MENU_VOICE:WOMAN}, {})
+  ).toEqual( WOMAN );
+});
+
+test('[SIMPLE-MENU-INPUT-VOICE-3] Read Default Voice from Script',
+() => {
+  expect(
+    funlet.input.getVoice({}, {})
+  ).toEqual( DEFAULT_VOICE );
+});
+
+test('[SIMPLE-MENU-INPUT-OPTIONS-1] Read Single Option from Event',
+() => {
+  expect(
+    funlet.input.getOptions({}, {Options:ACTION0})
+  ).toEqual( {"0":ACTION0} );
+});
+
+test('[SIMPLE-MENU-INPUT-OPTIONS-2] Read Sequential List of Options from Event',
+() => {
+  expect(
+    funlet.input.getOptions({}, {Options:[ACTION0,ACTION1,ACTION2]})
+  ).toEqual( {"0":ACTION0,"1":ACTION1,"2":ACTION2} );
+});
+
+test('[SIMPLE-MENU-INPUT-OPTIONS-3] Read List of Keys/Actions from Event',
+() => {
+  expect(
+    funlet.input.getOptions({}, {
+      "Options[42]":ACTION42,
+      "Options[101]":ACTION101,
+      "Options[1234]":ACTION1234
+    })
+  ).toEqual( {"42":ACTION42,"101":ACTION101,"1234":ACTION1234} );
+});
+
+test('[SIMPLE-MENU-INPUT-OPTIONS-4] Read Single Option from Environment',
+() => {
+  expect(
+    funlet.input.getOptions({
+      FUNLET_MENU_OPTION7_URL: ACTION7
+    }, {})
+  ).toEqual( {"7":ACTION7} );
+});
+
+test('[SIMPLE-MENU-INPUT-OPTIONS-5] Read Single Key/Action from Environment',
+() => {
+  expect(
+    funlet.input.getOptions({
+      FUNLET_MENU_OPTION4_DIGITS: 42,
+      FUNLET_MENU_OPTION4_URL: ACTION42
+    }, {})
+  ).toEqual( {"42":ACTION42} );
+});
+
+test('[SIMPLE-MENU-INPUT-OPTIONS-6] Read Key/Action List from Environment',
+() => {
+  expect(
+    funlet.input.getOptions({
+      FUNLET_MENU_OPTION1_DIGITS: "42",
+      FUNLET_MENU_OPTION1_URL: ACTION42,
+
+      FUNLET_MENU_OPTION2_DIGITS: "101",
+      FUNLET_MENU_OPTION2_URL: ACTION101,
+
+      FUNLET_MENU_OPTION3_DIGITS: "1234",
+      FUNLET_MENU_OPTION3_URL: ACTION1234
+    }, {})
+  ).toEqual( {"42":ACTION42,"101":ACTION101,"1234":ACTION1234} );
+});
+
+test('[SIMPLE-MENU-INPUT-OPTIONS-7] Read Default Options from Script',
+() => {
+  expect(
+    funlet.input.getOptions({},{})
+  ).toEqual( DEFAULT_OPTIONS );
+});
+
+test('[SIMPLE-MENU-INPUT-DIGITS-0] Read No Digits from Event',
+() => {
+  expect(
+    funlet.input.getDigits({},{})
+  ).toEqual( NO_DIGITS );
+});
+
+test('[SIMPLE-MENU-INPUT-DIGITS-1] Read Non-Empty Digits from Event',
+() => {
+  expect(
+    funlet.input.getDigits({},{Digits:NON_EMPTY_DIGITS})
+  ).toEqual( NON_EMPTY_DIGITS );
+});
+
+test('[SIMPLE-MENU-OUTPUT-GATHER-DIGITS-0] Gather Digits Without Message',
+() => {
+  const EMPTY_MESSAGE="";
+  const DIGITS = 5;
+  const GATHER_DIGITS =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="'+DIGITS+'"/>'+
+    '</Response>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  let gather = funlet.output.gatherDigits(
+    response, DIGITS, EMPTY_MESSAGE, DEFAULT_LANGUAGE, DEFAULT_VOICE
+  );
+  expect( response.toString() ).toEqual( GATHER_DIGITS );
+});
+
+test('[SIMPLE-MENU-OUTPUT-GATHER-DIGITS-1] '+
+     'Gather Digits With Recorded Message',
+() => {
+  const RECORDED_MESSAGE="https://example.com/recorded-message.mp3";
+  const DIGITS = 5;
+  const PLAY_MESSAGE_GATHER_DIGITS =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="'+DIGITS+'">'+
+        '<Play>'+RECORDED_MESSAGE+'</Play>'+
+      '</Gather>'+
+    '</Response>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  let gather = funlet.output.gatherDigits(
+    response, DIGITS, RECORDED_MESSAGE, DEFAULT_LANGUAGE, DEFAULT_VOICE
+  );
+  expect( response.toString() ).toEqual( PLAY_MESSAGE_GATHER_DIGITS );
+});
+
+test('[SIMPLE-MENU-OUTPUT-GATHER-DIGITS-2] Gather Digits With Text Message',
+() => {
+  const DIGITS = 5;
+  const SAY_MESSAGE_GATHER_DIGITS =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="'+DIGITS+'">'+
+        '<Say language="'+FRENCH+'" voice="'+WOMAN+'">'+TEXT_MESSAGE+'</Say>'+
+      '</Gather>'+
+    '</Response>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  let gather = funlet.output.gatherDigits(
+    response, DIGITS, TEXT_MESSAGE, FRENCH, WOMAN
+  );
+  expect( response.toString() ).toEqual( SAY_MESSAGE_GATHER_DIGITS );
+});
+
+test('[SIMPLE-MENU-OUTPUT-SIMPLE-MENU-1-3] Multiple Digits to Gather',
+() => {
+  const DIGITS = "12345".length;
+  const GATHER_MULTIPLE_DIGITS =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="'+DIGITS+'">'+
+        '<Say language="'+FRENCH+'" voice="'+WOMAN+'">'+TEXT_MESSAGE+'</Say>'+
+      '</Gather>'+
+      '<Redirect/>'+
+    '</Response>';
+  let response = new Twilio.twiml.VoiceResponse();
+  funlet.output.simpleMenuStage1(
+    response, TEXT_MESSAGE, FRENCH, WOMAN, {"12345":ACTION12345}
+  );
+  expect( response.toString() ).toEqual( GATHER_MULTIPLE_DIGITS );
+});
+
+test('[SIMPLE-MENU-OUTPUT-SIMPLE-MENU-2-1] Digits Pressed Match an Option',
+() => {
+  const REDIRECT_TO_MATCHING_OPTION =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Redirect>'+ACTION12345+'</Redirect>'+
+    '</Response>';
+  let response = new Twilio.twiml.VoiceResponse();
+  let hasMatch = funlet.output.simpleMenuStage2(
+    response, "12345", {"12345":ACTION12345}, ERROR_MESSAGE, FRENCH, MAN
+  );
+  expect( hasMatch ).toBe( true );
+  expect( response.toString() ).toEqual( REDIRECT_TO_MATCHING_OPTION );
+});
+
+test('[SIMPLE-MENU-OUTPUT-SIMPLE-MENU-2-2] '+
+     'Digits Pressed Do Not Match Any Option',
+() => {
+  const PLAY_ERROR_MESSAGE =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Play>'+ERROR_MESSAGE+'</Play>'+
+    '</Response>';
+  let response = new Twilio.twiml.VoiceResponse();
+  let hasMatch = funlet.output.simpleMenuStage2(
+    response, "42", {"12345":ACTION12345}, ERROR_MESSAGE, FRENCH, MAN
+  );
+  expect( hasMatch ).toBe( false );
+  expect( response.toString() ).toEqual( PLAY_ERROR_MESSAGE );
+});
 
 test.skip('Missing Tests', done => {
   const callback = (err, result) => {
     expect(result).toBe('...');
     done();
   };
-  funlet({}, {}, callback);
+  funlet.handler({}, {}, callback);
 });
