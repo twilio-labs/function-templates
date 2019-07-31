08e15fb7fabfcc437c57bcf53f1447eabffd2b4a
Phil Nash <philnash@gmail.com>
2019-07-31 14:47:55 +1000

    Adds simple conference template from Console templates.

---
+++ conference/tests/conference.test.js
@@ -0,0 +1,34 @@
+const conference = require('../functions/conference').handler;
+const helpers = require('../../test/test-helper');
+
+describe('simple conference', () => {
+  const context = {};
+  const event = {};
+  beforeAll(() => {
+    helpers.setup(context);
+  });
+
+  afterAll(() => {
+    helpers.teardown();
+  });
+
+  it('returns a VoiceResponse', done => {
+    const callback = (err, result) => {
+      expect(result).toBeInstanceOf(Twilio.twiml.VoiceResponse);
+      done();
+    };
+
+    conference(context, event, callback);
+  });
+
+  it('connects a call to a conference called Snowy Owl', done => {
+    const callback = (err, result) => {
+      expect(result.toString()).toMatch(
+        '<Dial><Conference>Snowy Owl</Conference></Dial>'
+      );
+      done();
+    };
+
+    conference(context, event, callback);
+  });
+});
