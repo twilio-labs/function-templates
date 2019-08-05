244d75504a2432d11c47f05759ed8b0f71ef6fdc
Eric Bréchemier <twilio@eric.brechemier.name>
2019-08-05 23:32:08 +0200

    Add unit tests and implementation of Whisper Funlet

--- funlet-whisper/funlet-whisper.js
+++ funlet-whisper/funlet-whisper.js
@@ -6,8 +6,60 @@
 
 // ## Script Parameters
 
+// recording URL or text message to say,
+// e.g. asking the recipient to press a key to accept the call
+const MY_MESSAGE = fromNumber =>
+`You are receiving a call from ${fromNumber}. Press any key to accept.`;
+
+// language code for conversion of text-to-speech messages,
+// e.g. 'en' or 'en-gb'
+const MY_LANGUAGE = "en";
+
+// voice for text-to-speech messages, one of 'man', 'woman' or 'alice'
+const MY_VOICE = "alice";
+
+// whether to request the recipient to press a key to accept the call
+const MY_HUMAN_CHECK = false;
+
 // ## Input
 exports.input = {};
+
+function getMessage(env, params) {
+  const caller = params.From || params.Caller || "";
+  return params.Message ||
+    env.FUNLET_WHISPER_MESSAGE ||
+    MY_MESSAGE( spell(caller) );
+}
+exports.input.getMessage = getMessage;
+
+function getLanguage(env, params) {
+  return params.Language || env.FUNLET_WHISPER_LANGUAGE || MY_LANGUAGE;
+}
+exports.input.getLanguage = getLanguage;
+
+function getVoice(env, params) {
+  return params.Voice || env.FUNLET_WHISPER_VOICE || MY_VOICE;
+}
+exports.input.getVoice = getVoice;
+
+function getHumanCheck(env, params) {
+  if ( typeof params.HumanCheck === "string" ) {
+    return params.HumanCheck === "true";
+  }
+  if ( typeof env.FUNLET_WHISPER_HUMAN_CHECK === "string" ) {
+    return env.FUNLET_WHISPER_HUMAN_CHECK === "true";
+  }
+  return MY_HUMAN_CHECK;
+}
+exports.input.getHumanCheck = getHumanCheck;
+
+function getDigits(env, params) {
+  if ( typeof params.Digits === "string" ) {
+   return params.Digits;
+  }
+  return null;
+}
+exports.input.getDigits = getDigits;
 
 // ## Dependencies
 const Twilio = require('twilio');
@@ -15,8 +67,117 @@ const Twilio = require('twilio');
 // ## Output
 exports.output = {};
 
+/*
+  Function: spell()
+  Spell a phone number digit by digit
+
+  Parameter:
+    numberString - string, phone number to spell
+
+  Returns:
+    string, the original phone number with each digit followed with a pause
+    (marked as '. ' for text-to-speech synthesis)
+*/
+function spell( numberString ) {
+  const PAUSE = '. ';
+  return numberString.split('').join(PAUSE)+PAUSE;
+}
+exports.output.spell = spell;
+
+// Copied from Simple Message Funlet
+function simpleMessage(response, message, language, voice) {
+  if ( message.length === 0 ) {
+    return;
+  }
+  if ( message.startsWith("http") ) {
+    response.play({}, message);
+  } else {
+    response.say({language:language, voice:voice}, message);
+  }
+}
+exports.output.simpleMessage = simpleMessage;
+
+// Copied from Simple Menu Funlet
+function gatherDigits(response, maxDigits, message, language, voice) {
+  simpleMessage(
+    response.gather({numDigits: maxDigits}),
+    message,
+    language,
+    voice
+  );
+}
+exports.output.gatherDigits = gatherDigits;
+
+/*
+  Function: whisperStage1()
+
+  Parameters:
+    * response - Twilio.twiml.VoiceResponse, Twilio Voice response in progress
+    * humanCheck - boolean, whether to request the recipient to press a key
+                   to accept the call explicitly
+    * message - string, recorded message (URL starting with 'http') to play
+                or text message to say
+    * language - string, language for text messages, e.g. 'en' for English
+                 with an American accent or 'en-gb' for English with a British
+                 accent. Use the voice 'alice' for the largest list of
+                 supported languages and accents.
+    * voice - string, voice for text messages,
+              one of 'man', 'woman' or 'alice'.
+
+  Response:
+    The input response is modified with instructions to gather one digit and,
+    when a human check is requested, to hang up if no digits have been pressed.
+*/
+function whisperStage1(response, humanCheck, message, language, voice) {
+  gatherDigits(response, 1, message, language, voice);
+  if ( humanCheck ) {
+    response.hangup();
+  }
+}
+exports.output.whisperStage1 = whisperStage1;
+
+/*
+  Function: whisperStage2()
+
+  Parameters:
+    * response - Twilio.twiml.VoiceResponse, Twilio Voice response in progress
+    * digits - string of digits gathered, or null when no digits have been set
+
+  Response:
+    The input response is modified with instructions to hang up when an empty
+    string of digits has been provided. This is a corner case, which is not
+    expected to happen in recent version versions of the Twilio API but has
+    been preserved for compatibility with the behavior of the original Twimlet.
+    When a digit has been pressed, or when digits have not been set, the
+    response is left unchanged.
+
+  Returns:
+    boolean, whether digits have been set
+*/
+function whisperStage2(response, digits) {
+  if ( digits === null ) {
+    return false;
+  }
+  if ( !digits ) {
+    response.hangup();
+  }
+  return true;
+}
+exports.output.whisperStage2 = whisperStage2;
+
 exports.handler = function(env, params, reply) {
   const NO_ERROR = null;
-  throw Error("Not implemented!");
-  reply(NO_ERROR, 'response');
+
+  let
+    response = new Twilio.twiml.VoiceResponse(),
+    digits = getDigits(env, params),
+    humanCheck = getHumanCheck(env, params),
+    message = getMessage(env, params),
+    language = getLanguage(env, params),
+    voice = getVoice(env, params);
+
+  if ( !whisperStage2(response, digits) ) {
+    whisperStage1(response, humanCheck, message, language, voice);
+  }
+  reply(NO_ERROR, response);
 };
--- funlet-whisper/funlet-whisper.test.js
+++ funlet-whisper/funlet-whisper.test.js
@@ -1,9 +1,273 @@
-const funlet = require('./funlet-whisper').handler;
+const funlet = require('./funlet-whisper');
+const Twilio = require('twilio');
 
-test.skip('Missing Tests', done => {
+const FROM_NUMBER="+19165550123";
+const SPELLED_FROM_NUMBER="+. 1. 9. 1. 6. 5. 5. 5. 0. 1. 2. 3. ";
+
+const RECORDED_MESSAGE="https://example.com/recorded-message.mp3";
+const TEXT_MESSAGE="Text message";
+const MESSAGE=TEXT_MESSAGE;
+const DEFAULT_MESSAGE=
+  'You are receiving a call from '+SPELLED_FROM_NUMBER+'. '+
+  'Press any key to accept.';
+
+const ENGLISH="en";
+const FRENCH="fr";
+const DEFAULT_LANGUAGE=ENGLISH;
+
+const MAN="man";
+const WOMAN="woman";
+const ALICE="alice";
+const DEFAULT_VOICE=ALICE;
+
+const NO_DIGITS = null;
+const EMPTY_DIGITS="";
+const NON_EMPTY_DIGITS="5";
+
+const XML_DECLARATION='<?xml version="1.0" encoding="UTF-8"?>';
+
+test('[WHISPER-INPUT-MESSAGE-1] Read Message from Event',
+() => {
+  expect(
+    funlet.input.getMessage({}, {Message:MESSAGE})
+  ).toEqual( MESSAGE );
+});
+
+test('[WHISPER-INPUT-MESSAGE-2] Read Message from Environment',
+() => {
+  expect(
+    funlet.input.getMessage({FUNLET_WHISPER_MESSAGE:MESSAGE}, {})
+  ).toEqual( MESSAGE );
+});
+
+test('[WHISPER-INPUT-MESSAGE-3] '+
+     'Read Default Message from Script (with number in From parameter)',
+() => {
+  expect(
+    funlet.input.getMessage({}, {From:FROM_NUMBER})
+  ).toEqual( DEFAULT_MESSAGE );
+});
+
+test('[WHISPER-INPUT-MESSAGE-4] '+
+     'Read Default Message from Script (with number in Caller parameter)',
+() => {
+  expect(
+    funlet.input.getMessage({}, {Caller:FROM_NUMBER})
+  ).toEqual( DEFAULT_MESSAGE );
+});
+
+test('[WHISPER-INPUT-LANGUAGE-1] Read Language from Event',
+() => {
+  expect(
+    funlet.input.getLanguage({}, {Language:FRENCH})
+  ).toEqual( FRENCH );
+});
+
+test('[WHISPER-INPUT-LANGUAGE-2] Read Language from Environment',
+() => {
+  expect(
+    funlet.input.getLanguage({FUNLET_WHISPER_LANGUAGE:FRENCH}, {})
+  ).toEqual( FRENCH );
+});
+
+test('[WHISPER-INPUT-LANGUAGE-3] Read Default Language from Script',
+() => {
+  expect(
+    funlet.input.getLanguage({}, {})
+  ).toEqual( DEFAULT_LANGUAGE );
+});
+
+test('[WHISPER-INPUT-VOICE-1] Read Voice from Event',
+() => {
+  expect(
+    funlet.input.getVoice({}, {Voice:MAN})
+  ).toEqual( MAN );
+});
+
+test('[WHISPER-INPUT-VOICE-2] Read Voice from Environment',
+() => {
+  expect(
+    funlet.input.getVoice({FUNLET_WHISPER_VOICE:WOMAN}, {})
+  ).toEqual( WOMAN );
+});
+
+test('[WHISPER-INPUT-VOICE-3] Read Default Voice from Script',
+() => {
+  expect(
+    funlet.input.getVoice({}, {})
+  ).toEqual( DEFAULT_VOICE );
+});
+
+test('[WHISPER-INPUT-HUMAN-CHECK-1] Read Human Check from Event',
+() => {
+  expect(
+    funlet.input.getHumanCheck({}, {HumanCheck:"true"})
+  ).toEqual( true );
+});
+
+test('[WHISPER-INPUT-HUMAN-CHECK-2] Read Human Check from Environment',
+() => {
+  expect(
+    funlet.input.getHumanCheck({FUNLET_WHISPER_HUMAN_CHECK:"true"}, {})
+  ).toEqual( true );
+});
+
+test('[WHISPER-INPUT-HUMAN-CHECK-3] Read Default Human Check from Script',
+() => {
+  const DEFAULT_HUMAN_CHECK = false;
+  expect(
+    funlet.input.getHumanCheck({}, {})
+  ).toEqual( DEFAULT_HUMAN_CHECK );
+});
+
+test('[WHISPER-INPUT-DIGITS-0] Read No Digits from Event',
+() => {
+  expect(
+    funlet.input.getDigits({},{})
+  ).toEqual( NO_DIGITS );
+});
+
+test('[WHISPER-INPUT-DIGITS-1] Read Empty Digits from Event',
+() => {
+  expect(
+    funlet.input.getDigits({},{Digits:EMPTY_DIGITS})
+  ).toEqual( EMPTY_DIGITS );
+});
+
+test('[WHISPER-INPUT-DIGITS-2] Read Non-Empty Digits from Event',
+() => {
+  expect(
+    funlet.input.getDigits({},{Digits:NON_EMPTY_DIGITS})
+  ).toEqual( NON_EMPTY_DIGITS );
+});
+
+test('[WHISPER-OUTPUT-SPELL-1] Spell a phone number digit by digit',
+() => {
+  expect(
+    funlet.output.spell( FROM_NUMBER)
+  ).toEqual( SPELLED_FROM_NUMBER );
+});
+
+test('[WHISPER-OUTPUT-WHISPER-1-2] Recorded Message',
+() => {
+  const NO_HUMAN_CHECK = false;
+  const WHISPER_TEXT_MESSAGE =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="1">'+
+        '<Say language="'+FRENCH+'" voice="'+WOMAN+'">'+TEXT_MESSAGE+'</Say>'+
+      '</Gather>'+
+    '</Response>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  funlet.output.whisperStage1(
+    response, NO_HUMAN_CHECK, TEXT_MESSAGE, FRENCH, WOMAN
+  );
+  expect( response.toString() ).toEqual( WHISPER_TEXT_MESSAGE );
+});
+
+test('[WHISPER-OUTPUT-WHISPER-1-4] Human Check',
+() => {
+  const HUMAN_CHECK = true;
+  const WHISPER_RECORDED_MESSAGE_HUMAN_CHECK =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="1">'+
+        '<Play>'+RECORDED_MESSAGE+'</Play>'+
+      '</Gather>'+
+      '<Hangup/>'+
+    '</Response>';
+
+  let response = new Twilio.twiml.VoiceResponse();
+  funlet.output.whisperStage1(
+    response, HUMAN_CHECK, RECORDED_MESSAGE, DEFAULT_LANGUAGE, DEFAULT_VOICE
+  );
+  expect( response.toString() ).toEqual(
+    WHISPER_RECORDED_MESSAGE_HUMAN_CHECK
+  );
+});
+
+test('[WHISPER-OUTPUT-WHISPER-2-0] No Digits Provided',
+() => {
+  const EMPTY_RESPONSE =
+    XML_DECLARATION+
+    '<Response/>';
+  let response = new Twilio.twiml.VoiceResponse();
+  let hasDigits = funlet.output.whisperStage2( response, NO_DIGITS );
+  expect( hasDigits ).toBe( false );
+  expect( response.toString() ).toEqual( EMPTY_RESPONSE );
+});
+
+test('[WHISPER-OUTPUT-WHISPER-2-1] A Digit was Pressed',
+() => {
+  const EMPTY_RESPONSE =
+    XML_DECLARATION+
+    '<Response/>';
+  let response = new Twilio.twiml.VoiceResponse();
+  let hasDigits = funlet.output.whisperStage2( response, NON_EMPTY_DIGITS );
+  expect( hasDigits ).toBe( true );
+  expect( response.toString() ).toEqual( EMPTY_RESPONSE );
+});
+
+test('[WHISPER-OUTPUT-WHISPER-2-2] No Digits were Pressed, Empty Digits Set',
+() => {
+  const HANG_UP =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Hangup/>'+
+    '</Response>';
+  let response = new Twilio.twiml.VoiceResponse();
+  let hasDigits = funlet.output.whisperStage2( response, EMPTY_DIGITS );
+  expect( hasDigits ).toBe( true );
+  expect( response.toString() ).toEqual( HANG_UP );
+});
+
+test('[WHISPER-1-1] Full Response: Recorded Message', done => {
+  const FULL_RESPONSE_WHISPER_1_1 =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="1">'+
+        '<Play>'+RECORDED_MESSAGE+'</Play>'+
+      '</Gather>'+
+    '</Response>';
+
   const callback = (err, result) => {
-    expect(result).toBe('...');
+    expect( result ).toBeInstanceOf( Twilio.twiml.VoiceResponse );
+    expect( result.toString() ).toEqual( FULL_RESPONSE_WHISPER_1_1 );
     done();
   };
-  funlet({}, {}, callback);
+  funlet.handler({}, {Message:RECORDED_MESSAGE}, callback);
+});
+
+test('[WHISPER-1-4] Full Response: Human Check', done => {
+  const FULL_RESPONSE_WHISPER_1_4 =
+    XML_DECLARATION+
+    '<Response>'+
+      '<Gather numDigits="1">'+
+        '<Say language="'+DEFAULT_LANGUAGE+'" voice="'+DEFAULT_VOICE+'">'+
+          TEXT_MESSAGE+
+        '</Say>'+
+      '</Gather>'+
+      '<Hangup/>'+
+    '</Response>';
+
+  const callback = (err, result) => {
+    expect( result ).toBeInstanceOf( Twilio.twiml.VoiceResponse );
+    expect( result.toString() ).toEqual( FULL_RESPONSE_WHISPER_1_4 );
+    done();
+  };
+  funlet.handler({}, {Message:TEXT_MESSAGE, HumanCheck:"true"}, callback);
+});
+
+test('[WHISPER-2-1] Full Response: A Digit was Pressed', done => {
+  const FULL_RESPONSE_WHISPER_2_1 =
+    XML_DECLARATION+
+    '<Response/>';
+
+  const callback = (err, result) => {
+    expect( result ).toBeInstanceOf( Twilio.twiml.VoiceResponse );
+    expect( result.toString() ).toEqual( FULL_RESPONSE_WHISPER_2_1 );
+    done();
+  };
+  funlet.handler({}, {Digits:NON_EMPTY_DIGITS}, callback);
 });
