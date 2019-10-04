07b4ff6cd4e1f8069ce105a322537fffd3d93e16
Ting Lew <ting.lew@gmail.com>
2019-10-04 10:32:22 -0400

    inverted condition to handle empty blacklists

--- blacklist-call/functions/blacklist-call.js
+++ blacklist-call/functions/blacklist-call.js
@@ -1,20 +1,21 @@
 exports.handler = function(context, event, callback) {
-    let blacklist = event.blacklist || context.BLACKLIST || '';
-    const blacklistArray = blacklist.toString().split(',').map(num => num.trim());
+  let blacklist = event.blacklist || context.BLACKLIST || "";
+  const blacklistArray = blacklist
+    .toString()
+    .split(",")
+    .map(num => num.trim());
 
-    let twiml = new Twilio.twiml.VoiceResponse();
-    let blocked = true;
-    if (blacklistArray.length) {
-      if (!blacklistArray.includes(event.From)) {
-        blocked = false;
-      }
-    }
-    if (blocked) {
-      twiml.reject();
-    }
-    else {
+  let twiml = new Twilio.twiml.VoiceResponse();
+  let blocked = false;
+  if (blacklistArray.length > 0 && blacklistArray.includes(event.From)) {
+    blocked = true;
+  }
+
+  if (blocked) {
+    twiml.reject();
+  } else {
     // Update this line to your response.
-    twiml.redirect('https://demo.twilio.com/docs/voice.xml');
-    }
-    callback(null, twiml);
-  };
+    twiml.redirect("https://demo.twilio.com/docs/voice.xml");
+  }
+  callback(null, twiml);
+};
