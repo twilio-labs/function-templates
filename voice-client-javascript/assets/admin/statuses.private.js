9de4b354b1d3c48b4fda919a1531a98a27194958
Craig Dennis <cdennis@twilio.com>
2020-04-14 13:59:36 -0700

    Adds statuses test

--- voice-client-javascript/assets/admin/statuses.private.js
+++ voice-client-javascript/assets/admin/statuses.private.js
@@ -1,5 +1,5 @@
-const assets = Runtime.getAssets();
 const { stripIndents } = require("common-tags");
+const assets = Runtime.getAssets();
 const { getCurrentEnvironment, urlForSiblingPage } = require(assets[
   "/admin/shared.js"
 ].path);
@@ -11,7 +11,9 @@ async function checkEnvironmentInitialization(context) {
     valid: false,
   };
   if (!environment) {
-    status.description = stripIndents`This application is **must be** deployed. 
+    status.description = stripIndents`
+    This application is **must be** deployed. 
+    
     To deploy this function, use the following command:
     
     \`\`\`bash
@@ -20,12 +22,15 @@ async function checkEnvironmentInitialization(context) {
     After it has been deployed, revisit this page in your deployed application.
     `;
   } else if (!process.env.INITIALIZED) {
-    status.description = stripIndents`The Twilio Client JavaScript Quickstart requires that you setup a few things on your account. 
+    status.description = stripIndents`
+    The Twilio Client JavaScript Quickstart requires that you setup a few things on your account. 
+    
     We've written some tools that will initialize the various parts to use this tool.
     
     To initialize your environment, click the button below.
     
-    We'll explain what each of these parts are after we get started.`;
+    We'll explain what each of these parts are after we get started.
+    `;
     status.actions = [
       {
         title: `Initialize your application for your environment, ${environment.uniqueName}`,
@@ -71,9 +76,11 @@ async function getTwiMLApplicationStatus(context) {
     }
   } else {
     const results = await client.applications.list({ friendlyName });
-    if (results.length === 1) {
+    if (results.length >= 1) {
       const app = results[0];
-      status.description = stripIndents`We found an existing [TwiML Application](https://www.twilio.com/console/voice/twiml/apps/${app.sid}) with the name of \`${friendlyName}\.
+      status.description = stripIndents`
+      We found an existing [TwiML Application](https://www.twilio.com/console/voice/twiml/apps/${app.sid}) with the name of \`${friendlyName}\.
+      
       Would you like to use this app?
       `;
       status.actions = [
@@ -93,9 +100,11 @@ async function getTwiMLApplicationStatus(context) {
         },
       ];
     } else {
-      status.description = stripIndents`We need to create a new TwiML Application. You can do this by clicking the button below.
+      status.description = stripIndents`
+      We need to create a new TwiML Application. You can do this by clicking the button below.
       
-      You can do this [via the API or CLI](https://www.twilio.com/docs/usage/api/applications?code-sample=code-create-a-new-application-within-your-account&code-language=curl&code-sdk-version=json).`;
+      You can do this [via the API or CLI](https://www.twilio.com/docs/usage/api/applications?code-sample=code-create-a-new-application-within-your-account&code-language=curl&code-sdk-version=json).
+      `;
       status.actions = [
         {
           title: "Create a new TwiML Application",
@@ -128,7 +137,8 @@ async function getCallerIdStatus(context) {
       status.valid = true;
       status.description = `Your CallerID is set to ${process.env.CALLER_ID}`;
     } else {
-      status.description = stripIndents`Your CallerID is set to ${process.env.CALLER_ID}, but that number is not yet verified.
+      status.description = stripIndents`
+      Your CallerID is set to ${process.env.CALLER_ID}, but that number is not yet verified.
       
       You can [verify it via the console](https://www.twilio.com/console/phone-numbers/verified), [CLI or API](https://www.twilio.com/docs/voice/api/outgoing-caller-ids).
       `;
@@ -176,10 +186,13 @@ async function getTwiMLApplicationIsWiredUp(context) {
         status.valid = true;
         status.description = `TwiML Application Voice URL: \`${expectedFn}\``;
       } else {
-        status.description = stripIndents`Your TwiML Application's ( [${app.friendlyName}](https://www.twilio.com/console/voice/twiml/apps/${app.sid}) ) current Incoming Voice Url is \`${app.voiceUrl}\`. 
+        status.description = stripIndents`
+        Your TwiML Application's ( [${app.friendlyName}](https://www.twilio.com/console/voice/twiml/apps/${app.sid}) ) current Incoming Voice Url is \`${app.voiceUrl}\`. 
+        
         To work in this environment the Incoming Voice Url should be set to \`${expectedFn}\`. 
         
-        You can update this by clicking the button below.`;
+        You can update this by clicking the button below.
+        `;
         status.actions = [
           {
             title: `Update TwiML App Incoming Voice Webhook`,
@@ -213,9 +226,11 @@ async function getAPIKeyAndSecretFromEnvStatus(context) {
       status.valid = true;
       status.description = `Your web application will mint AccessTokens using your [${key.friendlyName} API Key](https://www.twilio.com/console/voice/settings/api-keys/${process.env.API_KEY})`;
     } catch (err) {
-      status.description = stripIndents`Uh oh, unable to find your API Key \`${process.env.API_KEY}\`.
+      status.description = stripIndents`
+      Uh oh, unable to find your API Key \`${process.env.API_KEY}\`.
       
-      Please [double check your key](https://www.twilio.com/console/voice/settings/api-keys/) or create a new one.`;
+      Please [double check your key](https://www.twilio.com/console/voice/settings/api-keys/) or create a new one.
+      `;
       status.actions = [
         {
           title: "Generate a new REST API Key and Secret",
@@ -227,11 +242,13 @@ async function getAPIKeyAndSecretFromEnvStatus(context) {
       ];
     }
   } else {
-    status.description = stripIndents`This application uses a REST API Key and Secret to mint AccessTokens.
+    status.description = stripIndents`
+    This application uses a REST API Key and Secret to mint AccessTokens.
     
     If you already have an API Key created for this purpose you can set the environment values, \`API_KEY\` and \`API_SECRET\`.
     
-    Alternatively you can generate a new key by clicking the button below.`;
+    Alternatively you can generate a new key by clicking the button below.
+    `;
     status.actions = [
       {
         title: "Generate a new REST API Key and Secret",
@@ -248,10 +265,11 @@ async function getAPIKeyAndSecretFromEnvStatus(context) {
 async function getDefaultPasswordChanged(context) {
   const status = {
     title: "Default admin password has been changed",
-    valid: false
+    valid: false,
   };
   if (process.env.ADMIN_PASSWORD === "default") {
-    status.description = stripIndents`Please take a moment to change your admin password from the provided default password. 
+    status.description = stripIndents`
+    Please take a moment to change your admin password from the provided default password. 
     
     You can do this by editing the \`ADMIN_PASSWORD\` value in the \`.env\` in the root of this project.
     
@@ -263,7 +281,8 @@ async function getDefaultPasswordChanged(context) {
     `;
   } else {
     status.valid = true;
-    status.description = "You're all set. You can change this value in your `.env` file at anytime.";
+    status.description =
+      "You're all set. You can change this value in your `.env` file at anytime.";
   }
   return status;
 }
