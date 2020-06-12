f884a4a7a629be493d58a85c1d018d88e24e2d2d
Dominik Kundel <dkundel@twilio.com>
2020-06-11 17:13:49 -0700

    fix(blocklist): rename to blocklist

---
+++ blocklist-call/functions/blocklist-call.js
@@ -0,0 +1,21 @@
+exports.handler = function (context, event, callback) {
+  let blocklist = event.blocklist || context.BLOCKLIST || '';
+  const blocklistArray = blocklist
+    .toString()
+    .split(',')
+    .map((num) => num.trim());
+
+  let twiml = new Twilio.twiml.VoiceResponse();
+  let blocked = false;
+  if (blocklistArray.length > 0 && blocklistArray.includes(event.From)) {
+    blocked = true;
+  }
+
+  if (blocked) {
+    twiml.reject();
+  } else {
+    // Update this line to your response.
+    twiml.redirect('https://demo.twilio.com/docs/voice.xml');
+  }
+  callback(null, twiml);
+};
