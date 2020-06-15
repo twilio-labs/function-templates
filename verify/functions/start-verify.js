2466effc13d223cc0364bef3279d9fe1e24c5887
Kelley Robinson <robinske@users.noreply.github.com>
2020-06-15 15:50:31 -0400

    Fix typo - close string

--- verify/functions/start-verify.js
+++ verify/functions/start-verify.js
@@ -54,7 +54,7 @@ exports.handler = function(context, event, callback) {
       locale: locale
     })
     .then(verification => {
-      console.log(`Sent verification: '${verification.sid}');
+      console.log(`Sent verification: '${verification.sid}'`);
       response.setStatusCode(200);
       response.setBody({
         "success": true
