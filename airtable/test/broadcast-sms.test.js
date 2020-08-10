a48d4a344c69f3ca56b30252abf86b2897611bea
Dominik Kundel <dkundel@twilio.com>
2020-08-10 12:16:39 -0700

    test(broadcast-sms): fix tests to use TWILIO_PHONE_NUMBER

--- airtable/test/broadcast-sms.test.js
+++ airtable/test/broadcast-sms.test.js
@@ -4,41 +4,43 @@ const Twilio = require('twilio');
 
 const event = {};
 
-jest.mock("airtable", () => {
+jest.mock('airtable', () => {
   return jest.fn().mockImplementation(() => {
     return mockAirtableClient;
   });
 });
 
 const mockRecord = {
-  get: jest.fn(() => '+1234567890')
-}
+  get: jest.fn(() => '+1234567890'),
+};
 const mockAllRecords = [mockRecord];
 
 const mockAirtableClient = {
   base: jest.fn().mockImplementation(() => {
     return mockAirtableBase;
-  })
+  }),
 };
 
 const mockAirtableBase = {
-  table: jest.fn().mockImplementation(() => { return mockAirtableTable; })
-}
+  table: jest.fn().mockImplementation(() => {
+    return mockAirtableTable;
+  }),
+};
 
 const mockAirtableTable = {
-  select: jest.fn(() => { return mockAirtableQuery; })
-}
+  select: jest.fn(() => {
+    return mockAirtableQuery;
+  }),
+};
 
 const mockAirtableQuery = {
-  all: jest.fn(() =>
-    Promise.resolve(mockAllRecords)
-  )
-}
+  all: jest.fn(() => Promise.resolve(mockAllRecords)),
+};
 
 let shouldFail = false;
 const mockClient = {
   messages: {
-    create: jest.fn( async () => {
+    create: jest.fn(async () => {
       if (shouldFail) {
         throw new Error('failed to send mock sms');
       } else {
@@ -55,7 +57,7 @@ const context = {
   AIRTABLE_APIKEY: 'keyAbcD12efG3HijK',
   AIRTABLE_BASEID: 'appAbcD12efG3HijK',
   AIRTABLE_TABLENAME: 'Table 1',
-  MY_PHONE_NUMBER: 'TwilioNumber'
+  TWILIO_PHONE_NUMBER: 'TwilioNumber',
 };
 
 beforeAll(() => {
@@ -66,7 +68,7 @@ afterAll(() => {
   helpers.teardown();
 });
 
-test('returns a Response', done => {
+test('returns a Response', (done) => {
   const callback = (err, result) => {
     expect(result).toBeDefined();
     done();
@@ -75,9 +77,13 @@ test('returns a Response', done => {
   broadcastSms(context, event, callback);
 });
 
-test('sends an SMS message', done => {
+test('sends an SMS message', (done) => {
   const callback = (err, result) => {
-    expect(mockClient.messages.create).toHaveBeenCalledWith({ from: 'TwilioNumber', to:'+1234567890', body:'This is a broadcast message from Twilio.' });
+    expect(mockClient.messages.create).toHaveBeenCalledWith({
+      from: 'TwilioNumber',
+      to: '+1234567890',
+      body: 'This is a broadcast message from Twilio.',
+    });
     done();
   };
 
