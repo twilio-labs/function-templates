d68045d26fd31691c19d25dd6b48ef9ffe1ffc68
Dominik Kundel <dkundel@twilio.com>
2020-09-01 16:08:04 -0700

    feat(templates): add sms-notifications template

---
+++ sms-notifications/assets/index.js
@@ -0,0 +1,78 @@
+const recipientForm = document.getElementById('recipientForm');
+const sendNotificationForm = document.getElementById('sendNotificationForm');
+const newRecipientInput = document.getElementById('newRecipientInput');
+const recipientList = document.getElementById('recipients');
+const resultSection = document.getElementById('resultSection');
+
+const recipients = [];
+
+function addRecipient(phoneNumber) {
+  recipients.push(phoneNumber);
+  const newListItem = document.createElement('li');
+  newListItem.innerText = phoneNumber;
+  recipientList.appendChild(newListItem);
+}
+
+function clearForm(form) {
+  // only clearing the passcode and leaving the message for convience
+  form.passcode.value = '';
+}
+
+recipientForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (newRecipientInput.value) {
+    addRecipient(newRecipientInput.value);
+    newRecipientInput.value = '';
+  }
+});
+
+function sendMessages(form) {
+  const data = {
+    passcode: form.passcode.value,
+    message: form.message.value,
+    recipients: recipients.join(','),
+  };
+
+  clearForm(form);
+
+  fetch('send-messages', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        if (resp.status === 401) {
+          throw new Error('Invalid Passcode');
+        } else {
+          throw new Error(
+            'Unexpected error. Please check the logs for what went wrong.'
+          );
+        }
+      }
+    })
+    .then((body) => {
+      const successCount = body.result.reduce((currentCount, resultItem) => {
+        return resultItem.success ? currentCount + 1 : currentCount;
+      }, 0);
+
+      resultSection.innerText = `Sent ${successCount} of ${body.result.length} messages. Check logs for details`;
+    })
+    .catch((err) => {
+      resultSection.innerText = err.message;
+    });
+}
+
+sendNotificationForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (recipients.length === 0) {
+    resultSection.innerText = 'Please enter at least one phone number';
+  } else {
+    resultSection.innerText = 'Sending messages. One moment';
+    sendMessages(evt.target);
+  }
+});
---
+++ sms-notifications/functions/send-messages.js
@@ -0,0 +1,37 @@
+exports.handler = function (context, event, callback) {
+  const phoneNumbers = event.recipients.split(',').map((x) => x.trim());
+  const message = event.message;
+  const passcode = event.passcode;
+
+  if (passcode !== context.PASSCODE) {
+    const response = new Twilio.Response();
+    response.setStatusCode(401);
+    response.setBody('Invalid passcode');
+    return callback(null, response);
+  }
+
+  const client = context.getTwilioClient();
+  const allMessageRequests = phoneNumbers.map((to) => {
+    return client.messages
+      .create({
+        from: context.TWILIO_PHONE_NUMBER,
+        to: to,
+        body: message,
+      })
+      .then((msg) => {
+        return { success: true, sid: msg.sid };
+      })
+      .catch((err) => {
+        return { success: false, error: err.message };
+      });
+  });
+
+  Promise.all(allMessageRequests)
+    .then((result) => {
+      return callback(null, { result });
+    })
+    .catch((err) => {
+      console.error(err);
+      return callback('Failed to fetch messages');
+    });
+};
