f34519c624c7991a425383dba82f5a2e4a3fa464
Kelley Robinson <krobinson@twilio.com>
2020-09-15 19:27:09 -0400

    Sync magic-links template with master, already in main (#137)
    Co-authored-by: Kelley Robinson <3673341+robinske@users.noreply.github.com>

---
+++ magic-links/tests/start-verify.test.js
@@ -0,0 +1,55 @@
+const startVerifyFunction = require('../functions/start-verify').handler;
+const helpers = require('../../test/test-helper');
+
+const mockService = {
+  verifications: {
+    create: jest.fn(() => Promise.resolve({
+      sid: "my-new-sid"
+    }))
+  }
+}
+
+const mockClient = {
+  verify: {
+    services: jest.fn(() => mockService)
+  }
+}
+
+const testContext = {
+  VERIFY_SERVICE_SID: 'default',
+  DOMAIN_NAME: 'example.com',
+  PATH: 'verify.html',
+  getTwilioClient: () => mockClient
+};
+
+describe('verify/start-verification', () => {
+  beforeAll(() => {
+    helpers.setup({});
+  });
+  afterAll(() => {
+    helpers.teardown();
+  });
+
+  test('returns an error response when required parameters are missing', done => {
+    const callback = (err, result) => {
+      expect(result).toBeDefined();
+      expect(result._body.success).toEqual(false);
+      expect(result._body.error.message).toEqual("Missing parameter; please provide an email.");
+      expect(mockClient.verify.services).not.toHaveBeenCalledWith(testContext.VERIFY_SERVICE_SID);
+      done();
+    };
+    const event = {};
+    startVerifyFunction(testContext, event, callback);
+  });
+
+  test('returns success with valid request', done => {
+    const callback = (err, result) => {
+      expect(result).toBeDefined();
+      expect(result._body.success).toEqual(true);
+      expect(mockClient.verify.services).toHaveBeenCalledWith(testContext.VERIFY_SERVICE_SID);
+      done();
+    };
+    const event = { "to": "hello@example.com" }
+    startVerifyFunction(testContext, event, callback);
+  });
+});
