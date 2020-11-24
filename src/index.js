6dd64565ababe3692d69e11f3eb725c85907619b
Jordan Mulcahey <jmulcahey@twilio.com>
2020-11-24 13:21:51 -0800

    Automatic asset hosting for development server
    The development server now automatically crawls function template
    directories and automatically creates routes for all of the asset
    directories it finds.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@
 
 const express = require('express');
 const path = require('path');
+const FileHound = require('filehound');
 
 const app = express();
 const port = 3002;
 
-// The list of templates with Cypress tests
-const templates = ['verify'];
+// Generate a list of function templates directories with static assets
+const assetDirs = FileHound.create()
+                       .paths(path.resolve(__dirname, '..'))
+                       .directory()
+                       .match('assets')
+                       .depth(2)
+                       .discard('node_modules', 'cypress')
+                       .findSync();
 
-templates.forEach((template) => {
-    app.use(`/${template}`, express.static(path.resolve(__dirname, '..', template, 'assets')));
+assetDirs.forEach((assetDir) => {
+    const template = path.basename(path.resolve(assetDir, '..'));
+
+    app.use(`/${template}`, express.static(assetDir));
 });
+
 
 app.listen(port, () => {
     console.log(`Hosting all function templates at http://localhost:${port}`);
