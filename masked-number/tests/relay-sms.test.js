0df3a1ea4b682c0f74f0402ea3be204cdebe0c40
Jordan Mulcahey <jmulcahey@twilio.com>
2020-12-18 15:28:24 -0800

    feat(templates): create masked-number template
    The masked-number template relays SMS messages to and from a personal
    phone number, via a Twilio number. This is based on the code in the blog
    post at
    https://www.twilio.com/blog/sms-forwarding-and-responding-using-twilio-and-javascript

---
+++ masked-number/tests/relay-sms.test.js
@@ -0,0 +1,106 @@
+const relaySms = require('../functions/relay-sms.protected').handler;
+const helpers = require('../../test/test-helper');
+
+let shouldFail = false;
+const mockTwilioClient = {
+  messages: {
+    create: jest.fn(() => {
+      if (shouldFail) {
+        return Promise.reject('Expected test error');
+      }
+
+      return Promise.resolve({
+        sid: 'my-new-sid',
+      });
+    }),
+  },
+};
+
+const context = {
+  getTwilioClient: () => mockTwilioClient,
+  MY_PHONE_NUMBER: '+1112223333',
+};
+
+const baseEvent = {
+  From: context.MY_PHONE_NUMBER,
+  To: '+13334445555',
+  Body: '+16667778888: test message',
+};
+
+beforeAll(() => {
+  helpers.setup(context);
+});
+
+afterAll(() => {
+  helpers.teardown();
+});
+
+beforeEach(() => {
+  shouldFail = false;
+  mockTwilioClient.messages.create.mockClear();
+});
+
+describe('masked-number function template', () => {
+  it('should send a message to a given phone number', (done) => {
+    const event = { ...baseEvent };
+    const callback = (err, _result) => {
+      expect(err).toBeFalsy();
+      expect(mockTwilioClient.messages.create).toHaveBeenCalled();
+
+      done();
+    }
+
+    relaySms(context, event, callback);
+  });
+
+  it('should relay messages from other phone numbers', (done) => {
+    const event = { ...baseEvent,
+                    From: baseEvent.To,
+                    Body: 'test message',
+                  };
+    const callback = (err, result) => {
+      expect(err).toBeFalsy();
+      const twiml = result.toString();
+      expect(twiml).toMatch(
+        `<Message to="${context.MY_PHONE_NUMBER}">${event.From}: ${event.Body}</Message>`
+      );
+
+      done();
+    };
+
+    relaySms(context, event, callback);
+  });
+
+  it('should reject messages without a recipient specified', (done) => {
+    const event = { ...baseEvent,
+                    Body: 'no recipient specified',
+                  };
+    const callback = (err, result) => {
+      expect(err).toBeFalsy();
+      const twiml = result.toString();
+      expect(twiml).toMatch(
+        `<Message>You need to specify a recipient number and a ":" before the message.</Message>`
+      );
+
+      done();
+    };
+
+    relaySms(context, event, callback);
+  });
+
+  it('should send an error text on message send failure', (done) => {
+    shouldFail = true;
+    const event = { ...baseEvent };
+    const callback = (err, result) => {
+      expect(err).toBeFalsy();
+      const twiml = result.toString();
+      expect(twiml).toMatch(
+        `<Message>I couldn't understand the recipient phone number you entered.</Message>`
+      );
+
+      done();
+    };
+
+    relaySms(context, event, callback);
+  });
+});
