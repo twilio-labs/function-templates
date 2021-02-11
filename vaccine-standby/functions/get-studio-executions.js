7749ea95c268d70b2567960de724542de49847d2
Jeff Eiden <jeff.m.eiden@gmail.com>
2021-02-10 17:53:22 -0800

    Construct the full JSON object from the flow and successfully render it on the page

--- vaccine-standby/functions/get-studio-executions.js
+++ vaccine-standby/functions/get-studio-executions.js
@@ -16,20 +16,36 @@ exports.handler = function(context, event, callback) {
                                 const executionContext = execution_context.context;
                                 let age = executionContext.widgets.Age.inbound.Body;
                                 let name = executionContext.widgets.Name.inbound.Body;
+                                let phone_number = executionContext.trigger.message.From;
+                                let zip_code = executionContext.widgets.ZipCode.inbound.Body;
+                                let essential_worker = executionContext.widgets.EssentialWorker.inbound.Body;
+                                let work_from_home = executionContext.widgets.WorkFromHome.inbound.Body;
+                                let long_term_care = executionContext.widgets.LongTermCare.inbound.Body;
+                                let congregate_setting = executionContext.widgets.CongregateSetting.inbound.Body;
+                                let health_condition = executionContext.widgets.HealthCondition.inbound.Body;
+                                let notification_preference = executionContext.widgets.NotificationPreference.inbound.Body;
+                                let language_preference = executionContext.widgets.LanguagePreference.inbound.Body;
                                 
-                                let resident = { 'age': age, 'name': name }
-                                console.log(resident)
-
+                                let resident = { 
+                                    'name': name,
+                                    'phone_number': phone_number,
+                                    'age': age,
+                                    'zip_code': zip_code,
+                                    'essential_worker': essential_worker,
+                                    'work_from_home': work_from_home,
+                                    'long_term_care': long_term_care,
+                                    'congregate_setting': congregate_setting,
+                                    'health_condition': health_condition,
+                                    'notification_preference': notification_preference,
+                                    'language_preference': language_preference
+                                };
                                 resolve(resident);
                             })
                             .catch(err => reject(err));
                     });
                 });
                 
-                console.log(promises);
                 Promise.all(promises).then((data) => {
-                    console.log('All promsies done');
-                    console.log(data);
                     callback(null, data)
                 })
                 .catch(err => callback(err));
