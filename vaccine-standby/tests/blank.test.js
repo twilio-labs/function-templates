5ba2504c62316a94141bdafb2e8d97ed4aaa017c
Jeff Eiden <jeff.m.eiden@gmail.com>
2021-02-11 10:26:56 -0800

    Remove unused test files

--- vaccine-standby/tests/blank.test.js
+++
@@ -1,9 +0,0 @@
-const handler = require('../functions/blank').handler;
-
-describe('vaccine-standby-list function template', () => {
-  it('Calls callback with empty JSON', () => {
-    const callback = jest.fn();
-    handler({}, {}, callback);
-    expect(callback).toHaveBeenCalledWith(null, {});
-  });
-});
