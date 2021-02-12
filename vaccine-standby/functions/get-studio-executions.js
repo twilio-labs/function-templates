d460787db2caafec78eba4fb255f56641ab10b87
Jeff Eiden <jeff.m.eiden@gmail.com>
2021-02-11 16:09:59 -0800

    Made the get studio executions handler tolerant of incomplete flow executions

--- vaccine-standby/functions/get-studio-executions.js
+++ vaccine-standby/functions/get-studio-executions.js
@@ -13,35 +13,43 @@ exports.handler = function(context, event, callback) {
                             .executionContext()
                             .fetch()
                             .then(execution_context => {
-                                const executionContext = execution_context.context;
-                                let age = executionContext.widgets.Age.inbound.Body;
-                                let name = executionContext.widgets.Name.inbound.Body;
-                                let phone_number = executionContext.trigger.message.From;
-                                let zip_code = executionContext.widgets.ZipCode.inbound.Body;
-                                let essential_worker = executionContext.widgets.EssentialWorker.inbound.Body;
-                                let work_from_home = executionContext.widgets.WorkFromHome.inbound.Body;
-                                let long_term_care = executionContext.widgets.LongTermCare.inbound.Body;
-                                let congregate_setting = executionContext.widgets.CongregateSetting.inbound.Body;
-                                let health_condition = executionContext.widgets.HealthCondition.inbound.Body;
-                                let notification_preference = executionContext.widgets.NotificationPreference.inbound.Body;
-                                let language_preference = executionContext.widgets.LanguagePreference.inbound.Body;
+                                try {
+                                    const executionContext = execution_context.context;
+                                    let age = executionContext.widgets.Age.inbound.Body;
+                                    let name = executionContext.widgets.Name.inbound.Body;
+                                    let phone_number = executionContext.trigger.message.From;
+                                    let zip_code = executionContext.widgets.ZipCode.inbound.Body;
+                                    let essential_worker = executionContext.widgets.EssentialWorker.inbound.Body;
+                                    let work_from_home = executionContext.widgets.WorkFromHome.inbound.Body;
+                                    let long_term_care = executionContext.widgets.LongTermCare.inbound.Body;
+                                    let congregate_setting = executionContext.widgets.CongregateSetting.inbound.Body;
+                                    let health_condition = executionContext.widgets.HealthCondition.inbound.Body;
+                                    let notification_preference = executionContext.widgets.NotificationPreference.inbound.Body;
+                                    let language_preference = executionContext.widgets.LanguagePreference.inbound.Body;
+                                    
+                                    let resident = { 
+                                        'name': name,
+                                        'phone_number': phone_number,
+                                        'age': age,
+                                        'zip_code': zip_code,
+                                        'essential_worker': essential_worker,
+                                        'work_from_home': work_from_home,
+                                        'long_term_care': long_term_care,
+                                        'congregate_setting': congregate_setting,
+                                        'health_condition': health_condition,
+                                        'notification_preference': notification_preference,
+                                        'language_preference': language_preference
+                                    };
+                                    resolve(resident);
+                                } catch(e) {
+                                    resolve();
+                                }
                                 
-                                let resident = { 
-                                    'name': name,
-                                    'phone_number': phone_number,
-                                    'age': age,
-                                    'zip_code': zip_code,
-                                    'essential_worker': essential_worker,
-                                    'work_from_home': work_from_home,
-                                    'long_term_care': long_term_care,
-                                    'congregate_setting': congregate_setting,
-                                    'health_condition': health_condition,
-                                    'notification_preference': notification_preference,
-                                    'language_preference': language_preference
-                                };
-                                resolve(resident);
                             })
-                            .catch(err => reject(err));
+                            .catch(err => {
+                                console.log(err);
+                                reject(err);
+                            });
                     });
                 });
                 
