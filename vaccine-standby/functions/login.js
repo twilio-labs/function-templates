105856c8fdb02dc962f646852d404ebf46e8c2b0
Charlie Weems <charlie.weems@gmail.com>
2021-02-12 10:35:38 -0800

    Linting, split out js and css

--- vaccine-standby/functions/login.js
+++ vaccine-standby/functions/login.js
@@ -1,35 +1,34 @@
-const crypto = require("crypto");
-const path = require("path");
-const SALT = 'salty'
+const crypto = require('crypto');
 
+// eslint-disable-next-line func-names
 exports.handler = function (context, event, callback) {
-    // Create a token from the password, and use it to check by setting it
-    console.log(event.password);
-    function createToken(context, password) {
-        const tokenString = `${context.ACCOUNT_SID}:${password}:${SALT}`;
+  // Create a token from the password, and use it to check by setting it
+  function createToken(password) {
+    const tokenString = `${context.ACCOUNT_SID}:${password}:${context.SALT}`;
 
-        // Similar to TwilioClient
-        return crypto
-            .createHmac("sha1", context.AUTH_TOKEN)
-            .update(Buffer.from(tokenString, "utf-8"))
-            .digest("base64");
-    }
+    return crypto
+      .createHmac('sha1', context.AUTH_TOKEN)
+      .update(Buffer.from(tokenString, 'utf-8'))
+      .digest('base64');
+  }
 
-    function isAllowed(token) {
-        // Create the token with the environment password
-        const masterToken = createToken(context, process.env.ADMIN_PASSWORD);
-        return masterToken === token;
-    }
+  function isAllowed(token) {
+    // Create the token with the environment password
+    const masterToken = createToken(context.ADMIN_PASSWORD);
+    return masterToken === token;
+  }
 
-    const token = createToken(context, event.password);
-    // Short-circuits
-    if (isAllowed(token)) {
-        return callback(null, { token });
-    }
+  const token = createToken(event.password);
+  // Short-circuits
 
-    response = new Twilio.Response();
-    response.setStatusCode(403);
-    response.setBody({'message': 'Unauthorized'});
+  if (isAllowed(token)) {
+    callback(null, { token });
+  }
 
-    callback(null, response);
+  // eslint-disable-next-line no-undef
+  const response = new Twilio.Response();
+  response.setStatusCode(403);
+  response.setBody({ message: 'Unauthorized' });
+
+  callback(null, response);
 };
--- vaccine-standby/functions/return-config.js
+++ vaccine-standby/functions/return-config.js
@@ -1,8 +1,10 @@
-exports.handler = function(context, event, callback) {
-  let phone_number = context.TWILIO_PHONE_NUMBER;
-  let response = new Twilio.Response();
+// eslint-disable-next-line func-names
+exports.handler = function (context, event, callback) {
+  const phone_number = context.TWILIO_PHONE_NUMBER;
+  // eslint-disable-next-line no-undef
+  const response = new Twilio.Response();
   response.setStatusCode(200);
   response.appendHeader('Content-Type', 'application/json');
-  response.setBody({'phone_number': phone_number});
+  response.setBody({ phone_number });
   callback(null, response);
 };
