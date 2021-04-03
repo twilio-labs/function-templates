75c29905addcf3b08364c133bc910c83543884cd
Anna Griffin <anna.griffin@students.olin.edu>
2021-04-03 17:59:20 -0400

    delete wrong spelling directory

--- volunteer-communication-cetner/tests/blank.test.js
+++
@@ -1,9 +0,0 @@
-const blank = require('../functions/blank').handler;
-
-describe('blank function template', () => {
-  it('Calls callback with empty JSON', () => {
-    const callback = jest.fn();
-    blank({}, {}, callback);
-    expect(callback).toHaveBeenCalledWith(null, {});
-  });
-});
