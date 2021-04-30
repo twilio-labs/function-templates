0c58b4e138564875e209edba66421a7eef5b3040
Jordan Mulcahey <jmulcahey@twilio.com>
2021-04-29 18:03:21 -0700

    feat: make funlet-simple-menu Quick Deployable

--- test/all-templates.test.js
+++ test/all-templates.test.js
@@ -9,7 +9,7 @@ const { parser } = require('configure-env');
 
 // skipList is a list of function templates that don't pass verification
 // for now, but will in the long term
-const skipList = ['conversations', 'funlet-simple-menu',
+const skipList = ['conversations',
                   'funlet-simple-message', 'funlet-simulring',
                   'funlet-whisper', 'vaccine-standby'];
 const excludedPaths = ['node_modules', 'test', 'coverage', 'docs', 'blank'] + skipList;
