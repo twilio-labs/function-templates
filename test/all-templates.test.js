f2c6ae841fe4d2b37ca2cbf8cc7f44d91872091c
Jordan Mulcahey <jmulcahey@twilio.com>
2021-04-29 17:08:52 -0700

    feat: make funlet-echo Quick Deployable

--- test/all-templates.test.js
+++ test/all-templates.test.js
@@ -9,8 +9,7 @@ const { parser } = require('configure-env');
 
 // skipList is a list of function templates that don't pass verification
 // for now, but will in the long term
-const skipList = ['conversations', 'funlet-echo',
-                  'funlet-find-me', 'funlet-simple-menu',
+const skipList = ['conversations', 'funlet-find-me', 'funlet-simple-menu',
                   'funlet-simple-message', 'funlet-simulring',
                   'funlet-whisper', 'vaccine-standby'];
 const excludedPaths = ['node_modules', 'test', 'coverage', 'docs', 'blank'] + skipList;
