a8a872d664c5f8b97daf97b05afebea73022eb43
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-05-04 11:07:47 -0400

    Add support to list factors

---
+++ verify-push-backend/functions/list-factors.js
@@ -0,0 +1,56 @@
+/**
+
+ */
+
+const assets = Runtime.getAssets();
+const { detectMissingParams } = require(assets["/missing-params.js"].path);
+
+exports.handler = function (context, event, callback) {
+  const response = new Twilio.Response();
+  response.appendHeader("Content-Type", "application/json");
+
+  // uncomment to support CORS
+  // response.appendHeader('Access-Control-Allow-Origin', '*');
+  // response.appendHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  // response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  const missingParams = detectMissingParams(["identity"], event);
+  if (missingParams.length > 0) {
+    response.setStatusCode(400);
+    response.setBody({
+      error: {
+        message: `Missing parameter; please provide: '${missingParams.join(
+          ", "
+        )}'.`,
+        moreInfo:
+          "https://www.twilio.com/docs/verify/api/challenge#create-a-challenge-resource",
+      },
+    });
+    return callback(null, response);
+  }
+
+  const client = context.getTwilioClient();
+  const serviceSid = context.VERIFY_SERVICE_SID;
+
+  client.verify
+    .services(serviceSid)
+    .entities(event.identity)
+    .factors.list({ limit: 20 })
+    .then((factors) => {
+      console.log(factors);
+      response.setStatusCode(200);
+      response.setBody(factors);
+      callback(null, response);
+    })
+    .catch((error) => {
+      console.error(error);
+      response.setStatusCode(error.status);
+      response.setBody({
+        error: {
+          message: error.message,
+          moreInfo: error.moreInfo,
+        },
+      });
+      callback(null, response);
+    });
+};
