d68c91c4871539ee37e18f07be6033128942e5e0
Jordan Mulcahey <jmulcahey@twilio.com>
2021-06-09 03:59:44 -0700

    feat(eslint): Make stripe-sms-receipt lint cleanly

--- stripe-sms-receipt/functions/send-sms-receipt.js
+++ stripe-sms-receipt/functions/send-sms-receipt.js
@@ -15,28 +15,36 @@ exports.handler = async function (context, event, callback) {
   const response = new Twilio.Response();
   response.appendHeader('Content-Type', 'application/json');
 
-  // Uncomment to support CORS.
-  // response.appendHeader('Access-Control-Allow-Origin', '*');
-  // response.appendHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  // response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+  /*
+   * Uncomment to support CORS.
+   * response.appendHeader('Access-Control-Allow-Origin', '*');
+   * response.appendHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+   * response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+   */
 
   // Only action charge.succeeded events
   if (event.type === 'charge.succeeded') {
-    // Retrieve the event object from Stripe to verify its validity.
-    // If you're running your own server environment you can verify
-    // the webhook signature instead:
-    // https://stripe.com/docs/webhooks/signatures#verify-official-libraries
+    /*
+     * Retrieve the event object from Stripe to verify its validity.
+     * If you're running your own server environment you can verify
+     * the webhook signature instead:
+     * https://stripe.com/docs/webhooks/signatures#verify-official-libraries
+     */
     try {
-      // To use stripe in your Twilio function you need to add it in the dependencies
-      // section of your functions config: https://www.twilio.com/console/functions/configure.
+      /*
+       * To use stripe in your Twilio function you need to add it in the dependencies
+       * section of your functions config: https://www.twilio.com/console/functions/configure.
+       */
       const stripe = Stripe(context.STRIPE_SECRET_KEY);
       const stripeEvent = await stripe.events.retrieve(event.id);
       const charge = await stripe.charges.retrieve(stripeEvent.data.object.id, {
         expand: ['customer'],
       });
-      const customer = charge.customer;
-      // Look for phone number on the customer otherwise use phone number from the
-      // billing details of the charge.
+      const { customer } = charge;
+      /*
+       * Look for phone number on the customer otherwise use phone number from the
+       * billing details of the charge.
+       */
       const customerPhone = customer
         ? customer.phone
         : charge.billing_details.phone;
@@ -54,16 +62,20 @@ exports.handler = async function (context, event, callback) {
       }
     } catch (error) {
       console.log(error.message);
-      // Respond with status code 400 so Stripe will retry the request:
-      // https://stripe.com/docs/webhooks/best-practices#retry-logic.
+      /*
+       * Respond with status code 400 so Stripe will retry the request:
+       * https://stripe.com/docs/webhooks/best-practices#retry-logic.
+       */
       response.setStatusCode(400);
       callback(null, response);
       return;
     }
   }
 
-  // Simply acknowledge unhandled events and payments without phone numbers
-  // with 200 status otherwise Stripe will retry the webhook event.
+  /*
+   * Simply acknowledge unhandled events and payments without phone numbers
+   * with 200 status otherwise Stripe will retry the webhook event.
+   */
   response.setStatusCode(200);
   callback(null, response);
 };
