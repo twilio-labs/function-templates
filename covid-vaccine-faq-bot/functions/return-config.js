6b3db9245d12d5c0619ed439edc8714034d0f1d9
iaggarwal27 <iaggarwal@twilio.com>
2021-06-16 18:47:55 -0400

    Update return-config.js

--- covid-vaccine-faq-bot/functions/return-config.js
+++ covid-vaccine-faq-bot/functions/return-config.js
@@ -1,11 +1,11 @@
 // eslint-disable-next-line func-names
 exports.handler = function (context, event, callback) {
-    const phone_number = context.TWILIO_PHONE_NUMBER;
+    const phoneNumber = context.TWILIO_PHONE_NUMBER;
     // eslint-disable-next-line no-undef
     const response = new Twilio.Response();
     response.setStatusCode(200);
     response.appendHeader('Content-Type', 'application/json');
-    response.setBody({ phone_number });
+    response.setBody({ phoneNumber });
     callback(null, response);
   };
   
