f87b4555c9bb7e8c5b6dd6ef54d6883186d5bfa3
iaggarwal27 <iaggarwal@twilio.com>
2021-06-16 18:45:07 -0400

    Update check-existing-flow.js

--- covid-vaccine-faq-bot/functions/check-existing-flow.js
+++ covid-vaccine-faq-bot/functions/check-existing-flow.js
@@ -7,14 +7,13 @@ exports.handler = async function(context, event, callback) {
         .then(flows => {
             if (flows.length > 0) {
                 flows.forEach(f => {
-                    // Note: If you are running this app locally and you kill your dev server after creating
-                    // the Studio Flow via the app, you will need to manually set FLOW_SID in your .env file
-                    // before the next time you start your dev server.
+                    /* Note: If you are running this app locally and you kill your dev server after creating
+                     the Studio Flow via the app, you will need to manually set FLOW_SID in your .env file
+                     before the next time you start your dev server. */
                     if (f.sid === flowSid) {
-                        callback(null, f.sid)
-                        return;
+                        callback(null, f.sid);
                     }
-                    callback(null, 'none')
+                    callback(null, 'none');
                 });
             } else {
                 callback(null, 'none');
