2b0cf2e987b56c62e6c916359c2bc21228477224
bochoi-twlo <bochoi@twilio.com>
2021-08-02 10:17:08 -0700

    2nd commit for HD-6

--- patient-appointment-management/functions/deployment/check-aws-bucket.js
+++ patient-appointment-management/functions/deployment/check-aws-bucket.js
@@ -27,27 +27,17 @@ exports.handler = async function (context, event, callback) {
       return callback(null, response);
     }
 
-    const AWS_S3_BUCKET = await getParam(context, 'AWS_S3_BUCKET');
-    const AWS_CF_STACK_BUCKET = await getParam(context, 'AWS_CF_STACK_BUCKET');
-    const DEPLOYER_AWS_ACCESS_KEY_ID = await getParam(
-      context,
-      'DEPLOYER_AWS_ACCESS_KEY_ID'
-    );
-    const DEPLOYER_AWS_SECRET_ACCESS_KEY = await getParam(
-      context,
-      'DEPLOYER_AWS_SECRET_ACCESS_KEY'
-    );
-    const AWS_REGION = await getParam(context, 'AWS_REGION');
-
     // ---------- get aws clients
     const options = {
-      accessKeyId: DEPLOYER_AWS_ACCESS_KEY_ID,
-      secretAccessKey: DEPLOYER_AWS_SECRET_ACCESS_KEY,
-      region: AWS_REGION,
+      accessKeyId: await getParam(context,'DEPLOYER_AWS_ACCESS_KEY_ID'),
+      secretAccessKey: await getParam(context,'DEPLOYER_AWS_SECRET_ACCESS_KEY'),
+      region: await getParam(context, 'AWS_REGION'),
     };
     const cf = new aws.CloudFormation(options);
 
     // ---------- look for dependent stack
+    const AWS_S3_BUCKET = await getParam(context, 'AWS_S3_BUCKET');
+    const AWS_CF_STACK_BUCKET = await getParam(context, 'AWS_CF_STACK_BUCKET');
     try {
       const response = await cf
         .describeStacks({ StackName: AWS_CF_STACK_BUCKET })
@@ -55,11 +45,16 @@ exports.handler = async function (context, event, callback) {
       const status = response.Stacks[0].StackStatus;
 
       console.log(THIS, 'StackStatus=', status);
-      if (status.endsWith('_COMPLETE')) return callback(null, AWS_S3_BUCKET);
+      if (status === 'CREATE_COMPLETE')
+        return callback(null, AWS_S3_BUCKET);
+      if (status === 'UPDATE_COMPLETE')
+        return callback(null, AWS_S3_BUCKET);
       else if (status.endsWith('_IN_PROGRESS'))
         return callback(null, 'DEPLOYING');
-      return callback(null, 'FAILED');
+      else
+        return callback(null, 'FAILED');
     } catch (AmazonCloudFormationException) {
+      // AWS will throw exception if matching stack is not found
       return callback(null, 'NOT-DEPLOYED');
     }
   } finally {
