07532cf11a181693a1d4ae658f5129646c4b84c2
Charlie Weems <charlie.weems@gmail.com>
2021-08-10 10:45:54 -0700

    Expand tests on receive-sms

--- optin-builder/functions/receive-sms.protected.js
+++ optin-builder/functions/receive-sms.protected.js
@@ -1,29 +1,15 @@
 exports.handler = function (context, event, callback) {
-  const stopKeywords = [
-    'cancel',
-    'end',
-    'quit',
-    'stop',
-    'stopall',
-    'unsubscribe',
-  ];
+  const stopKeywords = ['cancel', 'end', 'quit', 'stop', 'stopall', 'unsubscribe'];
   const startKeywords = ['start', 'unstop', 'yes'];
   const { path } = Runtime.getFunctions().utils;
-  const {
-    segmentOptIn,
-    webhookOptIn,
-    airtableCreateOptIn,
-    airtableRemoveOptIn,
-  } = require(path);
+  const { segmentOptIn, webhookOptIn, airtableCreateOptIn, airtableRemoveOptIn } = require(path);
 
   // Lowercase everything so that we're case insensitive
   const message = event.Body.toLowerCase();
   const optInKeyword = context.OPT_IN_KEYWORD.toLowerCase();
 
   const Airtable = require('airtable');
-  const base = new Airtable({ apiKey: context.AIRTABLE_API_KEY }).base(
-    context.AIRTABLE_BASE_ID
-  );
+  const base = new Airtable({ apiKey: context.AIRTABLE_API_KEY }).base(context.AIRTABLE_BASE_ID);
 
   const Analytics = require('analytics-node');
   const analytics = new Analytics(context.SEGMENT_WRITE_KEY);
@@ -46,7 +32,7 @@ exports.handler = function (context, event, callback) {
     if (context.DATA_SOURCE === 'airtable') {
       airtableRemoveOptIn(base, context, event, callback);
     } else if (context.DATA_SOURCE === 'segment') {
-      segmentOptIn(twiml, false, analytics, event, callback);
+      segmentOptIn(null, false, analytics, event, callback);
     } else if (context.DATA_SOURCE === 'webhook') {
       webhookOptIn(null, false, context, event, callback);
     } else {
