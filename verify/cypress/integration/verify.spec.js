69227dce3016a89f3a7a134d648ee8fd44a1aa5f
Andrea Zornada <azornada@twilio.com>
2022-01-07 18:49:38 +0000

    Cypress e2e tests

--- verify/cypress/integration/verify.spec.js
+++ verify/cypress/integration/verify.spec.js
@@ -1,56 +1,57 @@
+// eslint-disable-next-line
 /// <reference types="Cypress" />
 
 // `verify` function template UI-only tests
 
-const testNumber = "+18448144627";
-const testEmail = "test@example.org";
+const testNumber = '+18448144627';
+const testEmail = 'test@example.org';
 
-describe("verify Template UI", () => {
+describe('verify Template UI', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:4040");
+    cy.visit('http://localhost:4040');
   });
 
-  it("should identify itself as the Verify example", () => {
-    cy.contains("Twilio Verify");
+  it('should identify itself as the Verify example', () => {
+    cy.contains('Twilio Verify');
   });
 
-  it("should accept a number to verify via SMS by default", () => {
-    cy.get("[data-cy=input-phone]")
+  it('should accept a number to verify via SMS by default', () => {
+    cy.get('[data-cy=input-phone]')
       .type(testNumber)
-      .get("[data-cy=submit]")
+      .get('[data-cy=submit]')
       .click();
 
     // NOTE: This response will change when Cypress integration tests are incorporated
-    cy.contains("Error");
+    cy.contains('Error');
   });
 
-  it("should support verifying via a phone call", () => {
-    cy.get("[data-cy=radio-call]")
+  it('should support verifying via a phone call', () => {
+    cy.get('[data-cy=radio-call]')
       .click()
-      .get("[data-cy=input-phone]")
+      .get('[data-cy=input-phone]')
       .type(testNumber)
-      .get("[data-cy=submit]")
+      .get('[data-cy=submit]')
       .click();
 
     // NOTE: This response will change when Cypress integration tests are incorporated
-    cy.contains("Error");
+    cy.contains('Error');
   });
 
-  it("should support verifying via email", () => {
-    cy.get("[data-cy=radio-email]")
+  it('should support verifying via email', () => {
+    cy.get('[data-cy=radio-email]')
       .click()
-      .get("[data-cy=input-email]")
+      .get('[data-cy=input-email]')
       .type(testEmail)
-      .get("[data-cy=submit]")
+      .get('[data-cy=submit]')
       .click();
 
     // NOTE: This response will change when Cypress integration tests are incorporated
-    cy.contains("Error");
+    cy.contains('Error');
   });
 
-  it("should contain an EDIT_CODE tag and no APP_INFO", () => {
+  it('should contain an EDIT_CODE tag and no APP_INFO', () => {
     const collectComments = (doc, contents) => {
-      let commentWalker = doc.createTreeWalker(
+      const commentWalker = doc.createTreeWalker(
         doc.body,
         NodeFilter.SHOW_COMMENT,
         {
@@ -74,8 +75,8 @@ describe("verify Template UI", () => {
     };
 
     cy.document().then((doc) => {
-      expect(collectComments(doc, "EDIT_CODE").length).to.be.greaterThan(0);
-      expect(collectComments(doc, "APP_INFO").length).to.equal(0);
+      expect(collectComments(doc, 'EDIT_CODE').length).to.be.greaterThan(0);
+      expect(collectComments(doc, 'APP_INFO').length).to.equal(0);
     });
   });
 });
