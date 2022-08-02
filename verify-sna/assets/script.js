72385637c4de823c5f43cadd56817d2473990aa0
Elkin Narvaez <enarvaez@twilio.com>
2022-08-02 01:27:19 -0500

    Implement update table

--- verify-sna/assets/script.js
+++ verify-sna/assets/script.js
@@ -14,11 +14,83 @@ function clearStatus() {
   verificationsStatusSpan.textContent = '';
 }
 
+const verificationsTable = document.getElementById('verifications-table');
+
+const addAttributes = (element, attrObj) => {
+  for (let attr in attrObj) {
+    if (attrObj.hasOwnProperty(attr)) {
+      element.setAttribute(attr, attrObj[attr]);
+    }
+  }
+};
+
+const createCustomElement = (element, attributes, children) => {
+  let customElement = document.createElement(element);
+  if (children !== undefined) {
+    children.forEach((e) => {
+      if (e.nodeType) {
+        if (e.nodeType == 1 || e.nodeType == 11) {
+          customElement.appendChild(e);
+        }
+      } else {
+        customElement.innerHTML += e;
+      }
+    });
+  }
+  addAttributes(customElement, attributes);
+  return customElement;
+};
+
+function newTableRow(verification) {
+  // Phone number
+  let phoneNumber = document.createElement('td');
+  phoneNumber.innerHTML = verification.phone_number;
+
+  // Verification start datetime
+  let verificationStart = document.createElement('td');
+  verificationStart.innerHTML = verification.verification_start_datetime;
+
+  // Verification check datetime
+  let verificationCheck = document.createElement('td');
+  verificationCheck.innerHTML = verification.verification_check_datetime;
+
+  // Status
+  let statusSpan = document.createElement('span');
+  statusSpan.classList.add(verification.status);
+  if (verification.status === 'verified') {
+    statusSpan.innerHTML = 'Verified';
+  } else if (verification.status === 'pending') {
+    statusSpan.innerHTML = 'Pending';
+  } else if (verification.status === 'not-verified') {
+    statusSpan.innerHTML = 'Not verified';
+  } else {
+    statusSpan.innerHTML = 'Expired';
+  }
+  let status = createCustomElement('td', { class: 'status' }, [statusSpan]);
+
+  // Create new row
+  return createCustomElement('tr', {}, [
+    phoneNumber,
+    verificationStart,
+    verificationCheck,
+    status,
+  ]).outerHTML;
+}
+
+function updateVerificationsTable(verifications) {
+  let newTableBody = '';
+  verifications.forEach((verification) => {
+    newTableBody = `${newTableBody}${newTableRow(verification)}\n`;
+  });
+  verificationsTable.tBodies[0].innerHTML = newTableBody;
+  console.log(verificationsTable.tBodies[0]);
+}
+
 async function getVerifications(event) {
   event.preventDefault();
   console.log('Getting verifications...');
 
-  showVerificationsStatus('Getting verifications...');
+  // showVerificationsStatus('Getting verifications...');
 
   const data = new URLSearchParams();
 
@@ -31,11 +103,8 @@ async function getVerifications(event) {
     const json = await response.json();
 
     if (response.status === 200) {
-      showVerificationsStatus(json.message, { color: 'green' });
-      /**
-       * Note: Here I have to update the UI with the verifications that were retrieved. For now, all the verifications are being printed out
-       */
-      console.log(json.verifications);
+      // showVerificationsStatus(json.message, { color: 'green' });
+      updateVerificationsTable(json.verifications);
     } else {
       showVerificationsError(json.message);
     }
