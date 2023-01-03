f0a8f41ae22a44af62ba81a6526d19888af2cfe3
dkundel <dkundel@twilio.com>
2023-01-03 12:26:39 -0800

    wip: fix test run

--- experimental-flex-dialpad/tests/blank.test.js
+++
@@ -1,9 +0,0 @@
-const { handler } = require('../functions/blank');
-
-describe('flex-dialpad function template', () => {
-  it('Calls callback with empty JSON', () => {
-    const callback = jest.fn();
-    handler({}, {}, callback);
-    expect(callback).toHaveBeenCalledWith(null, {});
-  });
-});
