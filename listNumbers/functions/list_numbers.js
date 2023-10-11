3423499ff0c95e3830e7afdb74c70d86aca37e8c
dsTw5 <146445066+dsTw5@users.noreply.github.com>
2023-10-11 15:18:21 +0100

    Add files via upload

--- listNumbers/functions/list_numbers.js
+++ listNumbers/functions/list_numbers.js
@@ -6,23 +6,20 @@ exports.handler = async function (context, event, callback) {
   const authToken = process.env.AUTH_TOKEN;
   const subAccount = event.subAcc;
   const password = event.pass;
-  const pageSize = event.pageSize;
-  const page = event.page;
   const client = require("twilio")(accountSid, authToken, {
     accountSid: subAccount,
   });
-  const pageToken = event.pageToken;
 
   const response = new Twilio.Response();
 
   if (password !== process.env.Password) {
     finalData = { er: 0 };
     return callback(null, finalData);
-  } else if (pageSize > 0) {
+  } else if (event.pageSize > 0) {
       pageResp = await client.incomingPhoneNumbers.page({
-      pageSize: pageSize,
-      Page: page,
-      pageToken: pageToken,
+      pageSize: event.pageSize,
+      Page: event.page,
+      pageToken: event.pageToken,
     });
 
     return callback(null, pageResp);
