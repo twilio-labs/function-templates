b6632634eef91b8461905aa56307ad1b5d3cf8ce
dsumberac <146445066+dsTw5@users.noreply.github.com>
2023-11-01 17:08:57 +0000

    Add files via upload

--- transfers/functions/mask_account.js
+++ transfers/functions/mask_account.js
@@ -1,6 +1,6 @@
 exports.handler = async function (context, event, callback) {
-  let masked =
-    ("" + process.env.ACCOUNT_SID).slice(0, 28).replace(/./g, "*") + ("" + process.env.ACCOUNT_SID).slice(-6);
+  const masked =
+    (String(process.env.ACCOUNT_SID)).slice(0, 28).replace(/./g, "*") + (String(process.env.ACCOUNT_SID)).slice(-6);
   const accountSid = { acc: masked };
 
   try {
