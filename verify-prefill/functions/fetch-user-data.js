83879c5fe7dd3f5a27522e048b8f044e494f9c2c
Aric Day <aricwday@gmail.com>
2024-05-29 14:12:11 -0500

    Initial Commit
    verify-prefill sample app

---
+++ verify-prefill/functions/fetch-user-data.js
@@ -0,0 +1,27 @@
+exports.handler = async function (context, event, callback) {
+  const phoneNumber = event.phoneNumber;
+  const verificationSid = event.verificationSid;
+  const lookupApiKey = context.LOOKUP_API_KEY;
+  const lookupApiSecret = context.LOOKUP_API_SECRET;
+
+  try {
+    // Use dynamic import to load the node-fetch module
+    const fetch = (await import('node-fetch')).default;
+
+    const lookupUrl = `https://lookups.twilio.com/v2/PhoneNumbers/${phoneNumber}?Fields=pre_fill&VerificationSid=${verificationSid}`;
+    const lookupResponse = await fetch(lookupUrl, {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${lookupApiKey}:${lookupApiSecret}`
+        ).toString('base64')}`,
+      },
+    });
+
+    const lookupData = await lookupResponse.json();
+
+    return callback(null, { success: true, prefillData: lookupData.pre_fill });
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    return callback(null, { success: false, message: error.message });
+  }
+};
