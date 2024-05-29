79b7fff967f334249092835aa585e8ec44ee5f5f
Aric Day <aricwday@gmail.com>
2024-05-29 15:04:52 -0500

    eslint-disable

--- verify-prefill/assets/fetch-user-data.js
+++ verify-prefill/assets/fetch-user-data.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 exports.handler = async function (context, event, callback) {
   const phoneNumber = event.phoneNumber;
   const verificationSid = event.verificationSid;
