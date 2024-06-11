27fa16fc67437d1e9efef13a553f5fb2bffc9149
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2024-06-11 11:14:40 -0400

    Remove unused blank files

--- verify-prefill/tests/blank.test.js
+++
@@ -1,9 +0,0 @@
-const { handler } = require('../functions/blank');
-
-describe('verify-prefill function template', () => {
-  it('Calls callback with empty JSON', () => {
-    const callback = jest.fn();
-    handler({}, {}, callback);
-    expect(callback).toHaveBeenCalledWith(null, {});
-  });
-});
