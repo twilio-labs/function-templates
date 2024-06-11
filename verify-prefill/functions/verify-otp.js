ccbc282bbc55db44a98ed94d6e912bdd35ef3550
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2024-06-11 11:28:39 -0400

    Add test for verify-otp function

--- verify-prefill/functions/verify-otp.js
+++ verify-prefill/functions/verify-otp.js
@@ -2,14 +2,13 @@ exports.handler = async function (context, event, callback) {
   const { phoneNumber, code } = event;
   const { VERIFY_SERVICE_SID: serviceSid } = context;
 
-  const client = context.getTwilioClient();
-
-  if (!serviceSid) {
-    console.error('Missing VERIFY_SERVICE_SID');
-    return callback('Missing VERIFY_SERVICE_SID');
-  }
-
   try {
+    const client = context.getTwilioClient();
+
+    if (!serviceSid) {
+      throw new Error('Missing VERIFY_SERVICE_SID');
+    }
+
     // Verify the OTP using Twilio Verify API V2
     const verificationCheck = await client.verify.v2
       .services(serviceSid)
