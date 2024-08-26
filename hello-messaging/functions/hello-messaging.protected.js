67adb91efe127f6bee30ac4b9ad8b27f30ef730d
Rishab Kumar <rishabkumar7@gmail.com>
2024-08-26 13:54:19 -0400

    following comment best practices

--- hello-messaging/functions/hello-messaging.protected.js
+++ hello-messaging/functions/hello-messaging.protected.js
@@ -1,3 +1,25 @@
+/*
+ * Send a Hello World Message
+ * Description: When your Twilio Phone Number associated with this function
+ * receives a message, it will respond with "Hello World".
+ *
+ * Contents:
+ * 1. Main Handler
+ */
+
+/*
+ * 1. Main Handler
+ *
+ * This is the entry point to your Twilio Function,
+ * which will create a new Messaging Response using Twiml
+ * and use this to send a "Hello World" message back to the user.
+ * We then use the callback to return from your function
+ * with the Twiml Messaging Response you defined earlier.
+ * In the callback in non-error situations, the first
+ * parameter is null and the second parameter
+ * is the value you want to return.
+ */
+
 // Defining the handler function
 exports.handler = function (context, event, callback) {
   // Creating a new instance of Twilio's MessagingResponse object
