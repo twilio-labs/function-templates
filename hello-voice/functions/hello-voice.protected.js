f4b997fc653b4e71a88be96c8b32315cb67d6061
sbrij001 <shivani.brijmohan@gmail.com>
2024-09-08 13:39:48 -0400

    Added comments to the hello-voice.js file so that it's more comprehensive

--- hello-voice/functions/hello-voice.protected.js
+++ hello-voice/functions/hello-voice.protected.js
@@ -1,5 +1,14 @@
+/*
+ * hello-voice.protected.js
+ * This file contains an AWS Lambda function handler that generates a Twilio Voice Response. When triggered, it creates a TwiML response that instructs Twilio to speak "Hello World" to the caller. 
+ */
+
+// This is an AWS Lambda function handler for a Twilio Voice Response
 exports.handler = function (context, event, callback) {
+  // Create a new TwiML VoiceResponse object
   const twiml = new Twilio.twiml.VoiceResponse();
+  // Add a 'Say' verb to the TwiML response, which will speak the text 'Hello World' to the caller
   twiml.say('Hello World');
+  // End the function by calling the callback with the generated TwiML response
   callback(null, twiml);
 };
