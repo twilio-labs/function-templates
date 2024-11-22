9c52cb4f02654af337cb150f65ef7204106b44d7
Paul Kamp <pkamp3@users.noreply.github.com>
2024-11-22 15:24:46 -0500

    Update video-token.js
    Video token requires `identity`. See here: https://www.twilio.com/docs/video/tutorials/user-identity-access-tokens#generate-helper-lib

--- video/functions/video-token.js
+++ video/functions/video-token.js
@@ -4,6 +4,7 @@ exports.handler = function (context, event, callback) {
   const TWILIO_ACCOUNT_SID = context.ACCOUNT_SID;
   const TWILIO_API_KEY = context.API_KEY;
   const TWILIO_API_SECRET = context.API_SECRET;
+  const identity = 'user';
   const ACCESS_TOKEN_IDENTITY =
     Math.random().toString(36).substring(2, 15) +
     Math.random().toString(36).substring(2, 15); // random client name
@@ -28,7 +29,8 @@ exports.handler = function (context, event, callback) {
   const accessToken = new AccessToken(
     TWILIO_ACCOUNT_SID,
     TWILIO_API_KEY,
-    TWILIO_API_SECRET
+    TWILIO_API_SECRET,
+    {identity: identity}
   );
   accessToken.addGrant(videoGrant); // Add the grant to the token
   accessToken.identity = ACCESS_TOKEN_IDENTITY;
